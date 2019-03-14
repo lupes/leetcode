3125d979850fb9942dbf96b889744eaa31a70140
lupe <746696169@qq.com>
2019-03-14 23:45:22 +0800

    21模板文件提交

---
+++ question_001_100/question_21_30/21_merge_two_lists.go
@@ -0,0 +1,12 @@
+package question_0011_0020
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	return nil
+}
