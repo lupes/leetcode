afd36a5fe95d8946b6c0b7409cc783f34d76a860
lupe <lupe.luo@klook.com>
2019-03-16 13:56:01 +0800

    18 四数之和模版

---
+++ question_001_100/question_11_20/18_four_sum.go
@@ -0,0 +1,5 @@
+package question_11_20
+
+func fourSum(nums []int, target int) [][]int {
+	return nil
+}
---
+++ question_001_100/question_11_20/18_four_sum_test.go
@@ -0,0 +1,27 @@
+package question_11_20
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_fourSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fourSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fourSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
