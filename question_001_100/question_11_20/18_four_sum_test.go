d4e761813effa4e270a4c2b7973d926d964167a6
lupe <lupe.luo@klook.com>
2019-03-19 22:51:36 +0800

    18 调整测试结果顺序保证测试通过

--- question_001_100/question_11_20/18_four_sum_test.go
+++ question_001_100/question_11_20/18_four_sum_test.go
@@ -15,11 +15,12 @@ func Test_fourSum(t *testing.T) {
 		args args
 		want [][]int
 	}{
-		{"test#1", args{[]int{1, 0, -1, 0, -2, 2}, 0}, [][]int{{-1, 0, 0, 1}, {-2, -1, 1, 2}, {-2, 0, 0, 2}}},
+		{"test#1", args{[]int{1, 0, -1, 0, -2, 2}, 0}, [][]int{{-2, -1, 1, 2}, {-2, 0, 0, 2}, {-1, 0, 0, 1}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := fourSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+			got := fourSum(tt.args.nums, tt.args.target)
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("fourSum() = %v, want %v", got, tt.want)
 			}
 		})
