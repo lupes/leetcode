a8b537ad525e355a3f3d62e98e518345af685a73
lupe <lupe.luo@klook.com>
2019-03-20 20:31:03 +0800

    25 k个一组翻转链表模版

---
+++ question_001_100/question_21_30/25_reverse_k_group.go
@@ -0,0 +1,5 @@
+package question_0011_0020
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	return head
+}
---
+++ question_001_100/question_21_30/25_reverse_k_group_test.go
@@ -0,0 +1,46 @@
+package question_0011_0020
+
+import (
+	"reflect"
+	"testing"
+)
+
+var head1 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
+var head2 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
+var head3 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
+var head4 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
+var head5 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
+var head6 *ListNode
+
+var want2 = &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{Val: 5, Next: nil}}}}}
+var want3 = &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
+var want4 = &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: &ListNode{Val: 5, Next: nil}}}}}
+var want5 = &ListNode{Val: 5, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: nil}}}}}
+var want6 *ListNode
+
+func Test_reverseKGroup(t *testing.T) {
+
+	type args struct {
+		head *ListNode
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{"test#1", args{head: head1, k: 1}, head1},
+		{"test#2", args{head: head2, k: 2}, want2},
+		{"test#3", args{head: head3, k: 3}, want3},
+		{"test#4", args{head: head4, k: 4}, want4},
+		{"test#5", args{head: head5, k: 5}, want5},
+		{"test#6", args{head: head6, k: 6}, want6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseKGroup(tt.args.head, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseKGroup() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
