1f0945347a636163a590b838eb0aaf86c964ba15
lupe <lupe.luo@klook.com>
2019-03-20 21:17:47 +0800

    25 k个一组翻转链表

--- question_001_100/question_21_30/25_reverse_k_group.go
+++ question_001_100/question_21_30/25_reverse_k_group.go
@@ -1,5 +1,43 @@
 package question_0011_0020
 
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	return head
+	if head == nil || k == 1 {
+		return head
+	}
+
+	var (
+		array = make([]*ListNode, k, k)
+		res   *ListNode
+		last  *ListNode
+		next  = head
+		index = 0
+	)
+
+	for next != nil {
+		array[index] = next
+		index++
+		next = next.Next
+		if index == k {
+			for i := k - 1; i >= 0; i-- {
+				if res == nil {
+					res = array[i]
+				} else {
+					last.Next = array[i]
+				}
+				last = array[i]
+			}
+			index = 0
+		}
+	}
+	for i := 0; i < index; i++ {
+		if res == nil {
+			res = array[i]
+		} else {
+			last.Next = array[i]
+		}
+		last = array[i]
+	}
+	last.Next = nil
+
+	return res
 }
--- question_001_100/question_21_30/25_reverse_k_group_test.go
+++ question_001_100/question_21_30/25_reverse_k_group_test.go
@@ -10,13 +10,16 @@ var head2 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Ne
 var head3 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
 var head4 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
 var head5 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
-var head6 *ListNode
+var head6 = &ListNode{Val: 1, Next: nil}
+var head7 *ListNode
+var head8 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: nil}}
 
 var want2 = &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{Val: 5, Next: nil}}}}}
 var want3 = &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}}
 var want4 = &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: &ListNode{Val: 5, Next: nil}}}}}
 var want5 = &ListNode{Val: 5, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: nil}}}}}
-var want6 *ListNode
+var want7 *ListNode
+var want8 = &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: nil}}
 
 func Test_reverseKGroup(t *testing.T) {
 
@@ -34,12 +37,25 @@ func Test_reverseKGroup(t *testing.T) {
 		{"test#3", args{head: head3, k: 3}, want3},
 		{"test#4", args{head: head4, k: 4}, want4},
 		{"test#5", args{head: head5, k: 5}, want5},
-		{"test#6", args{head: head6, k: 6}, want6},
+		{"test#6", args{head: head6, k: 2}, head6},
+		{"test#7", args{head: head7, k: 6}, want7},
+		{"test#8", args{head: head8, k: 2}, want8},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := reverseKGroup(tt.args.head, tt.args.k); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("reverseKGroup() = %v, want %v", got, tt.want)
+			got := reverseKGroup(tt.args.head, tt.args.k)
+			want := tt.want
+			var arr1 []int
+			var arr2 []int
+			for got != nil && want != nil {
+				arr1 = append(arr1, got.Val)
+				arr2 = append(arr2, want.Val)
+				got = got.Next
+				want = want.Next
+			}
+
+			if !reflect.DeepEqual(arr1, arr2) {
+				t.Errorf("reverseKGroup() = %v, want %v", arr1, arr2)
 			}
 		})
 	}
