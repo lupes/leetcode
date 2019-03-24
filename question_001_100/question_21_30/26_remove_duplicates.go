d2df4e4078fb45a68ed4d00aceae83db852e649f
lupe <lupe.luo@klook.com>
2019-03-24 17:17:24 +0800

    26 删除排序数组中的重复项

--- question_001_100/question_21_30/26_remove_duplicates.go
+++ question_001_100/question_21_30/26_remove_duplicates.go
@@ -1,5 +1,17 @@
 package question_0011_0020
 
 func removeDuplicates(nums []int) int {
-	return 0
+	size := len(nums)
+	if size < 2 {
+		return size
+	}
+	offset := 0
+	for i := 1; i < size; i++ {
+		if nums[i] == nums[i-1-offset] {
+			offset += 1
+		} else {
+			nums[i-offset] = nums[i]
+		}
+	}
+	return size - offset
 }
