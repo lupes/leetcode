8985b2793ea67c6f69aecbdf1232fcd721d86d84
lupe <lupe.luo@klook.com>
2019-03-27 19:44:20 +0800

    30 串联所有单词的子串测试更新

--- question_001_100/question_21_30/30_find_sub_string_test.go
+++ question_001_100/question_21_30/30_find_sub_string_test.go
@@ -15,13 +15,12 @@ func Test_findSubstring(t *testing.T) {
 		args args
 		want []int
 	}{
-		{"test#0", args{nil, nil}, nil},
-		{"test#1", args{nil, []string{}}, nil},
-		{"test#2", args{"", []string{}}, nil},
-		{"test#3", args{"", []string{""}}, nil},
-		{"test#4", args{"i", []string{"i"}}, []int{0}},
-		{"test#5", args{"ii", []string{"i"}}, []int{0, 1}},
-		{"test#6", args{"barfoothefoobarman", []string{"foo", "bar"}}, []int{0, 9}},
+		{"test#0", args{"", nil}, nil},
+		{"test#1", args{"", []string{}}, nil},
+		{"test#2", args{"", []string{""}}, nil},
+		{"test#3", args{"i", []string{"i"}}, []int{0}},
+		{"test#4", args{"ii", []string{"i"}}, []int{0, 1}},
+		{"test#5", args{"barfoothefoobarman", []string{"foo", "bar"}}, []int{0, 9}},
 		{"test#6", args{"wordgoodgoodgoodbestword", []string{"word", "good", "best", "word"}}, []int{}},
 	}
 	for _, tt := range tests {
