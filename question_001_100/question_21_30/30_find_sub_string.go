5f36fb28361fdf342cefc9979f71e0cae61e5fb3
lupe <lupe.luo@klook.com>
2019-03-28 11:31:44 +0800

    30 串联所有单词的子串解法二

--- question_001_100/question_21_30/30_find_sub_string.go
+++ question_001_100/question_21_30/30_find_sub_string.go
@@ -1,8 +1,8 @@
 package question_0011_0020
 
 import (
+	"reflect"
 	"sort"
-	"strings"
 )
 
 func findSubstring(s string, words []string) []int {
@@ -15,55 +15,22 @@ func findSubstring(s string, words []string) []int {
 	if words[0] == "" {
 		return nil
 	}
-	wordMap := make(map[string]int)
-	var values []string
-	for _, v := range words {
-		n, ok := wordMap[v]
-		wordMap[v] = n + 1
-		if !ok {
-			values = append(values, v)
-		}
-	}
-
+	var res []int
+	sort.Strings(words)
+	size := len(s)
 	wordCount := len(words)
 	wordLen := len(words[0])
-	tmp := make(map[int]string)
-	var keys []int
-	for _, value := range values {
-		index := 0
-		for {
-			j := strings.Index(s[index:], value)
-			if j > -1 {
-				tmp[j+index] = value
-				keys = append(keys, j+index)
-				index = j + index + 1
-			} else {
-				break
-			}
+	wordsLen := wordCount * wordLen
+	tmp := make([]string, wordCount, wordCount)
+	for i := 0; i < size-wordsLen+1; i++ {
+		for j := 0; j < wordCount; j++ {
+			start := i + j*wordLen
+			tmp[j] = s[start : start+wordLen]
 		}
-	}
-	var res []int
-	sort.Ints(keys)
-L:
-	for _, key := range keys {
-		w := tmp[key]
-		t := make(map[string]int)
-		t[w] = 1
-		for i := 1; i < wordCount; i++ {
-			j := key + i*wordLen
-			v, ok := tmp[j]
-			if !ok {
-				continue L
-			} else {
-				t[v] = t[v] + 1
-			}
+		sort.Strings(tmp)
+		if reflect.DeepEqual(tmp, words) {
+			res = append(res, i)
 		}
-		for _, v := range values {
-			if t[v] != wordMap[v] {
-				continue L
-			}
-		}
-		res = append(res, key)
 	}
 	return res
 }
--- question_001_100/question_21_30/30_find_sub_string_test.go
+++ question_001_100/question_21_30/30_find_sub_string_test.go
@@ -2,6 +2,7 @@ package question_0011_0020
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -27,7 +28,9 @@ func Test_findSubstring(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findSubstring(tt.args.s, tt.args.words); !reflect.DeepEqual(got, tt.want) {
+			got := findSubstring(tt.args.s, tt.args.words)
+			sort.Ints(got)
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("findSubstring() = %v, want %v", got, tt.want)
 			}
 		})
