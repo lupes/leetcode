33e39dee8b703f210d16ebd388f9838307c8d37c
lupe <lupe.luo@klook.com>
2019-03-29 23:05:57 +0800

    39 组合总和

--- question_001_100/question_31_40/39_combination_sum.go
+++ question_001_100/question_31_40/39_combination_sum.go
@@ -1,5 +1,32 @@
 package question_31_40
 
+import (
+	"sort"
+)
+
 func combinationSum(candidates []int, target int) [][]int {
-	return nil
+	count := len(candidates)
+	if count == 0 {
+		return nil
+	}
+	sort.Ints(candidates)
+	var res [][]int
+	find(&res, nil, candidates, target, 0)
+	return res
+}
+
+func find(res *[][]int, tmp, candidates []int, target, index int) {
+	if target < candidates[0] {
+		return
+	}
+	for i := index; i < len(candidates) && target >= candidates[i]; i++ {
+		t := make([]int, len(tmp))
+		copy(t, tmp)
+		t = append(t, candidates[i])
+		if target-candidates[i] == 0 {
+			*res = append(*res, t)
+			continue
+		}
+		find(res, t, candidates, target-candidates[i], i)
+	}
 }
--- question_001_100/question_31_40/39_combination_sum_test.go
+++ question_001_100/question_31_40/39_combination_sum_test.go
@@ -19,6 +19,8 @@ func Test_combinationSum(t *testing.T) {
 		{"test#1", args{[]int{1}, 2}, [][]int{{1, 1}}},
 		{"test#2", args{[]int{2, 3, 6, 7}, 7}, [][]int{{2, 2, 3}, {7}}},
 		{"test#3", args{[]int{2, 3, 5}, 8}, [][]int{{2, 2, 2, 2}, {2, 3, 3}, {3, 5}}},
+		{"test#4", args{[]int{2, 8, 5, 4}, 10}, [][]int{{2, 2, 2, 2, 2}, {2, 2, 2, 4}, {2, 4, 4}, {2, 8}, {5, 5}}},
+		{"test#5", args{[]int{7, 12, 5, 10, 9, 4, 6, 8}, 32}, [][]int{{2, 2, 2, 2, 2}, {2, 2, 2, 4}, {2, 4, 4}, {2, 8}, {5, 5}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
