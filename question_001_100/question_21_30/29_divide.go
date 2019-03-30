2a0d9a809a290db4d467b7ff6c407fe633b038b5
lupe <lupe.luo@klook.com>
2019-03-30 23:20:29 +0800

    29 两数相除测试模版

---
+++ question_001_100/question_21_30/29_divide.go
@@ -0,0 +1,5 @@
+package question_0011_0020
+
+func divide(dividend int, divisor int) int {
+	return 0
+}
---
+++ question_001_100/question_21_30/29_divide_test.go
@@ -0,0 +1,25 @@
+package question_0011_0020
+
+import "testing"
+
+func Test_divide(t *testing.T) {
+	type args struct {
+		dividend int
+		divisor  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test#0", args{10, 3}, 3},
+		{"test#1", args{7, -3}, -2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := divide(tt.args.dividend, tt.args.divisor); got != tt.want {
+				t.Errorf("divide() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
