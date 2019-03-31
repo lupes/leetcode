f8d99fd23a354d75d9f800a92f670a57b8a150b9
lupe <lupe.luo@klook.com>
2019-03-31 23:07:57 +0800

    28 实现strStr()

--- question_001_100/question_21_30/28_str_str.go
+++ question_001_100/question_21_30/28_str_str.go
@@ -1,5 +1,29 @@
 package question_0011_0020
 
 func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	hLen := len(haystack)
+	nLen := len(needle)
+	if nLen > hLen {
+		return -1
+	}
+	for i, c := range haystack {
+		if hLen-i < nLen {
+			break
+		}
+		if uint8(c) == needle[0] {
+			flag := true
+			for j := 0; j < nLen; j++ {
+				if haystack[i+j] != needle[j] {
+					flag = false
+				}
+			}
+			if flag {
+				return i
+			}
+		}
+	}
 	return -1
 }
