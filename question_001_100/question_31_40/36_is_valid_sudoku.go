b11d910314365ef8f8f1e744faf068b773b506af
lupe <lupe.luo@klook.com>
2019-04-01 20:08:59 +0800

    36 有效的数独优化

--- question_001_100/question_31_40/36_is_valid_sudoku.go
+++ question_001_100/question_31_40/36_is_valid_sudoku.go
@@ -1,23 +1,22 @@
 package question_31_40
 
 func isValidSudoku(board [][]byte) bool {
-	var flagsLine [9][9]bool
+	var flagsRow [9][9]bool
 	var flagsCol [9][9]bool
-	var flags [9][9]bool
-	for i, line := range board {
-		for j, num := range line {
+	var flagsSqu [9][9]bool
+	for i, row := range board {
+		for j, num := range row {
 			if num == '.' {
 				continue
 			}
 			num = num - '1'
 			k := (i/3)*3 + (j / 3)
-			if flagsLine[i][num] || flagsCol[j][num] || flags[k][num] {
+			if flagsRow[i][num] || flagsCol[j][num] || flagsSqu[k][num] {
 				return false
-			} else {
-				flagsLine[i][num] = true
-				flagsCol[j][num] = true
-				flags[k][num] = true
 			}
+			flagsRow[i][num] = true
+			flagsCol[j][num] = true
+			flagsSqu[k][num] = true
 		}
 	}
 	return true
