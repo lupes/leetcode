f88c96e59fb2b6787f1cfda2bc5732db99c7d961
lupe <lupe.luo@klook.com>
2019-04-01 19:58:56 +0800

    36 有效的数独

--- question_001_100/question_31_40/36_is_valid_sudoku.go
+++ question_001_100/question_31_40/36_is_valid_sudoku.go
@@ -1,5 +1,24 @@
 package question_31_40
 
 func isValidSudoku(board [][]byte) bool {
-	return false
+	var flagsLine [9][9]bool
+	var flagsCol [9][9]bool
+	var flags [9][9]bool
+	for i, line := range board {
+		for j, num := range line {
+			if num == '.' {
+				continue
+			}
+			num = num - '1'
+			k := (i/3)*3 + (j / 3)
+			if flagsLine[i][num] || flagsCol[j][num] || flags[k][num] {
+				return false
+			} else {
+				flagsLine[i][num] = true
+				flagsCol[j][num] = true
+				flags[k][num] = true
+			}
+		}
+	}
+	return true
 }
