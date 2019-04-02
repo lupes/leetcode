a2d9e5f2926b42109e42674d5a45ee19c3259c89
lupe <lupe.luo@klook.com>
2019-04-02 09:49:48 +0800

    29 两数相除

--- question_001_100/question_21_30/29_divide_test.go
+++ question_001_100/question_21_30/29_divide_test.go
@@ -1,23 +1,24 @@
 package question_0011_0020
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func Test_divide(t *testing.T) {
-	type args struct {
+	tests := []struct {
+		name     string
 		dividend int
 		divisor  int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
+		want     int
 	}{
-		{"test#0", args{10, 3}, 3},
-		{"test#1", args{7, -3}, -2},
+		{"test#0", 10, 3, 3},
+		{"test#1", 7, -3, -2},
+		{"test#2", math.MinInt32, -1, math.MaxInt32},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := divide(tt.args.dividend, tt.args.divisor); got != tt.want {
+			if got := divide(tt.dividend, tt.divisor); got != tt.want {
 				t.Errorf("divide() = %v, want %v", got, tt.want)
 			}
 		})
