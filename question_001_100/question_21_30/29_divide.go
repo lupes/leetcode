db55a4c43fd795c648c7932ffdd3cf8bb7632ce1
lupe <lupe.luo@klook.com>
2019-04-02 22:47:44 +0800

    29 两数相除

--- question_001_100/question_21_30/29_divide.go
+++ question_001_100/question_21_30/29_divide.go
@@ -1,5 +1,38 @@
 package question_0011_0020
 
+import (
+	"math"
+)
+
 func divide(dividend int, divisor int) int {
-	return 0
+	if dividend == 0 {
+		return 0
+	}
+	if dividend == math.MinInt32 && divisor == -1 {
+		return math.MaxInt32
+	}
+	res := 0
+	flag := dividend^divisor < 0
+	if dividend < 0 {
+		dividend = -dividend
+	}
+	if divisor < 0 {
+		divisor = -divisor
+	}
+	for i := byte(31); ; i-- {
+		if (dividend >> i) >= divisor {
+			res += 1 << i
+			dividend -= divisor << i
+		}
+		if dividend < divisor {
+			break
+		}
+		if i == 0 {
+			break
+		}
+	}
+	if flag {
+		res = -res
+	}
+	return res
 }
--- question_001_100/question_21_30/29_divide_test.go
+++ question_001_100/question_21_30/29_divide_test.go
@@ -14,7 +14,9 @@ func Test_divide(t *testing.T) {
 	}{
 		{"test#0", 10, 3, 3},
 		{"test#1", 7, -3, -2},
-		{"test#2", math.MinInt32, -1, math.MaxInt32},
+		{"test#2", -7, -3, 2},
+		{"test#3", -7, 3, -2},
+		{"test#4", math.MinInt32, -1, math.MaxInt32},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
