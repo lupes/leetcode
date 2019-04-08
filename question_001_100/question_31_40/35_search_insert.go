698f367f6d012673a6b97027ee97e00a95573efb
lupe <lupe.luo@klook.com>
2019-04-08 20:00:30 +0800

    35 搜索插入位置TDD

---
+++ question_001_100/question_31_40/35_search_insert.go
@@ -0,0 +1,5 @@
+package question_31_40
+
+func searchInsert(nums []int, target int) int {
+	return -1
+}
---
+++ question_001_100/question_31_40/35_search_insert_test.go
@@ -0,0 +1,28 @@
+package question_31_40
+
+import "testing"
+
+func Test_searchInsert(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test#0", args{[]int{}, 5}, 0},
+		{"test#1", args{[]int{1, 3, 5, 6}, 5}, 2},
+		{"test#2", args{[]int{1, 3, 5, 6}, 2}, 1},
+		{"test#3", args{[]int{1, 3, 5, 6}, 7}, 4},
+		{"test#4", args{[]int{1, 3, 5, 6}, 0}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchInsert(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("searchInsert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
