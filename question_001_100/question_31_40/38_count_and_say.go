cf33a685ed3ce77ea6d4dc15ff5baff27f79a7e8
lupe <lupe.luo@klook.com>
2019-04-08 21:08:25 +0800

    38 报数

--- question_001_100/question_31_40/38_count_and_say.go
+++ question_001_100/question_31_40/38_count_and_say.go
@@ -10,14 +10,14 @@ func countAndSay(n int) string {
 	for i := 2; i <= n; i++ {
 		c := 1
 		t := result[0]
-		for i := 1; i < len(result); i++ {
-			if t == result[i] {
+		for j := 1; j < len(result); j++ {
+			if t == result[j] {
 				c++
 				continue
 			}
 			tmp += strconv.Itoa(c) + string(t)
 			c = 1
-			t = result[i]
+			t = result[j]
 		}
 		result = tmp + strconv.Itoa(c) + string(t)
 		tmp = ""
