ba65de655618c4980c165bda1fdfa48b5db08e10
lupe <lupe.luo@klook.com>
2019-04-08 20:20:26 +0800

    35 搜索插入位置

--- question_001_100/question_31_40/35_search_insert.go
+++ question_001_100/question_31_40/35_search_insert.go
@@ -1,5 +1,12 @@
 package question_31_40
 
 func searchInsert(nums []int, target int) int {
-	return -1
+	i := -1
+	num := 0
+	for i, num = range nums {
+		if num >= target {
+			return i
+		}
+	}
+	return i + 1
 }
