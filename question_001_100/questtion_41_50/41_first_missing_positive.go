f869de6609bd0784d1761ac9ac71dbc7f90e117b
lupe <lupe.luo@klook.com>
2019-04-08 21:14:08 +0800

    41 缺失的第一个正数TDD

---
+++ question_001_100/questtion_41_50/41_first_missing_positive.go
@@ -0,0 +1,5 @@
+package questtion_41_50
+
+func firstMissingPositive(nums []int) int {
+	return 0
+}
---
+++ question_001_100/questtion_41_50/41_first_missing_positive_test.go
@@ -0,0 +1,23 @@
+package questtion_41_50
+
+import "testing"
+
+func Test_firstMissingPositive(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want int
+	}{
+		{"test#0", []int{}, 1},
+		{"test#1", []int{1, 2, 0}, 3},
+		{"test#2", []int{3, 4, -1, 1}, 2},
+		{"test#3", []int{7, 8, 9, 11, 12}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstMissingPositive(tt.nums); got != tt.want {
+				t.Errorf("firstMissingPositive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
