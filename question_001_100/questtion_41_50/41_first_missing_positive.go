e7f27d0e443569ccfd756db76487651f954f52b3
lupe <lupe.luo@klook.com>
2019-04-09 09:37:33 +0800

    41 缺失的第一个正数

--- question_001_100/questtion_41_50/41_first_missing_positive.go
+++ question_001_100/questtion_41_50/41_first_missing_positive.go
@@ -1,5 +1,22 @@
 package questtion_41_50
 
+import (
+	"sort"
+)
+
 func firstMissingPositive(nums []int) int {
-	return 0
+	sort.Ints(nums)
+	index := 1
+	for i, n := range nums {
+		if n > 0 {
+			if i > 0 && n == nums[i-1] {
+				continue
+			}
+			if n != index {
+				return index
+			}
+			index++
+		}
+	}
+	return index
 }
--- question_001_100/questtion_41_50/41_first_missing_positive_test.go
+++ question_001_100/questtion_41_50/41_first_missing_positive_test.go
@@ -12,6 +12,7 @@ func Test_firstMissingPositive(t *testing.T) {
 		{"test#1", []int{1, 2, 0}, 3},
 		{"test#2", []int{3, 4, -1, 1}, 2},
 		{"test#3", []int{7, 8, 9, 11, 12}, 1},
+		{"test#4", []int{0, 2, 2, 1, 1}, 3},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
