a22476102f5b18b6e7d090d1c8f212120e246a33
lupe <lupe.luo@klook.com>
2019-04-14 22:08:24 +0800

    46 全排列

--- question_001_100/question_41_50/46_permute.go
+++ question_001_100/question_41_50/46_permute.go
@@ -1,9 +1,27 @@
 package question_41_50
 
 func permute(nums []int) [][]int {
-	size := len(nums)
-	if size == 0 {
-		return [][]int{}
+	flag := make([]bool, len(nums))
+	return dfs1(nums, flag)
+}
+
+func dfs1(nums []int, flag []bool) [][]int {
+	var res [][]int
+	for i, num := range nums {
+		if !flag[i] {
+			var newFlag = make([]bool, len(flag))
+			copy(newFlag, flag)
+			newFlag[i] = true
+			tmp := dfs1(nums, newFlag)
+			newArr := []int{num}
+			if tmp == nil {
+				res = append(res, newArr)
+			}
+			for _, t := range tmp {
+				r := append(newArr, t...)
+				res = append(res, r)
+			}
+		}
 	}
-	return nil
+	return res
 }
