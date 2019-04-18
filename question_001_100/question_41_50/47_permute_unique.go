5c0d4ecff994b80713b63af516731c9cb6002ece
lupe <lupe.luo@klook.com>
2019-04-18 18:56:51 +0800

    67 全排列优化

--- question_001_100/question_41_50/47_permute_unique.go
+++ question_001_100/question_41_50/47_permute_unique.go
@@ -1,39 +1,27 @@
 package question_41_50
 
-import (
-	"fmt"
-	"sort"
-)
-
 func permuteUnique(nums []int) [][]int {
-	sort.Ints(nums)
-	tmp := permuteUniqueDfs(nums)
-	m := make(map[string][]int)
-	var res [][]int
-	for _, r := range tmp {
-		key := fmt.Sprintf("%v", r)
-		_, ok := m[key]
-		if !ok {
-			m[key] = r
-			res = append(res, r)
-		}
-	}
-	return res
-}
-
-func permuteUniqueDfs(nums []int) [][]int {
 	var res [][]int
 	if len(nums) == 1 {
 		return [][]int{nums}
 	}
+
+Continue:
 	for i, num := range nums {
-		if i > 0 && (nums[i-1] == num || num == nums[0]) {
-			continue
+		// 去重当某一个数字和前面数字相同时不交换
+		for _, t := range nums[:i] {
+			if t == num {
+				continue Continue
+			}
 		}
 		nums[0], nums[i] = nums[i], nums[0]
-		for _, t := range permuteUniqueDfs(nums[1:]) {
-			r := append([]int{num}, t...)
-			res = append(res, r)
+		if len(nums[1:]) == 1 {
+			res = append(res, []int{num, nums[1]})
+		} else {
+			for _, t := range permuteUnique(nums[1:]) {
+				r := append([]int{num}, t...)
+				res = append(res, r)
+			}
 		}
 		nums[0], nums[i] = nums[i], nums[0]
 	}
