569efbac9e76f0f9703487ed214cc3e0603c9444
lupe <lupe.luo@klook.com>
2019-04-19 08:54:05 +0800

    45 跳跃游戏II

--- question_001_100/question_41_50/45_jump.go
+++ question_001_100/question_41_50/45_jump.go
@@ -1,5 +1,26 @@
 package question_41_50
 
 func jump(nums []int) int {
+	size := len(nums)
+	if size == 1 {
+		return 0
+	}
+	var num, res, max, tmp, next, i, j int
+	for i = 0; i < size; {
+		res += 1
+		num = nums[i]
+		if num+i+1 >= size {
+			return res
+		}
+		max = 0
+		for j = 1; j <= num; j++ {
+			tmp = j + nums[j+i]
+			if tmp > max {
+				max = tmp
+				next = i + j
+			}
+		}
+		i = next
+	}
 	return 0
 }
--- question_001_100/question_41_50/45_jump_test.go
+++ question_001_100/question_41_50/45_jump_test.go
@@ -8,18 +8,19 @@ func Test_jump(t *testing.T) {
 		nums []int
 		want int
 	}{
-		{"test#0", []int{1}, 1},
-		{"test#1", []int{2, 1}, 1},
-		{"test#2", []int{2, 1, 3}, 2},
-		{"test#3", []int{2, 1, 3, 1}, 2},
-		{"test#4", []int{2, 1, 3, 1, 1}, 2},
-		{"test#5", []int{2, 1, 3, 1, 1, 1}, 2},
-		{"test#6", []int{2, 3, 1, 1, 4}, 2},
+		{"test#0", []int{0}, 0},
+		{"test#1", []int{1}, 0},
+		{"test#2", []int{2, 1}, 1},
+		{"test#3", []int{2, 1, 3}, 1},
+		{"test#4", []int{2, 1, 3, 1}, 2},
+		{"test#5", []int{2, 1, 3, 1, 1}, 2},
+		{"test#6", []int{2, 1, 3, 1, 1, 1}, 2},
+		{"test#7", []int{2, 3, 1, 1, 4}, 2},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := jump(tt.nums); got != tt.want {
-				t.Errorf("jump() = %v, want %v", got, tt.want)
+				t.Errorf("jump(%v) = %v, want %v", tt.nums, got, tt.want)
 			}
 		})
 	}
