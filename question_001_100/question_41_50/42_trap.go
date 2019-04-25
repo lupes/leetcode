0d51e8ee51e168899f521f407786fcc1a70f0477
lupe <lupe.luo@klook.com>
2019-04-25 20:15:01 +0800

    42 接雨水

--- question_001_100/question_41_50/42_trap.go
+++ question_001_100/question_41_50/42_trap.go
@@ -2,9 +2,34 @@ package question_41_50
 
 func trap(height []int) int {
 	size := len(height)
-	if size < 2 {
-		return 0
+	var l, r, res, h, max int
+	for _, t := range height {
+		if t > max {
+			max = t
+		}
 	}
-
-	return 0
+	for {
+		l, r = 0, size-1
+		h += 1
+		if max < h {
+			break
+		}
+		for l < r {
+			if height[l] < h {
+				l++
+			}
+			if height[r] < h {
+				r--
+			}
+			if height[l] >= h && height[r] >= h {
+				break
+			}
+		}
+		for i := l; i < r; i++ {
+			if height[i] < h {
+				res += 1
+			}
+		}
+	}
+	return res
 }
