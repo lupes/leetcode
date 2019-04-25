c9bf42248e0b5bfb7067ffd2c9959bfb144356a3
lupe <lupe.luo@klook.com>
2019-04-25 20:33:29 +0800

    42 接雨水优化

--- question_001_100/question_41_50/42_trap.go
+++ question_001_100/question_41_50/42_trap.go
@@ -1,35 +1,20 @@
 package question_41_50
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
 func trap(height []int) int {
 	size := len(height)
-	var l, r, res, h, max int
-	for _, t := range height {
-		if t > max {
-			max = t
-		}
+	var l, r, res int
+	for i := 0; i < size; i++ {
+		l = max(l, height[i])
+		r = max(l, height[size-1-i])
+		res += l + r - height[i]
 	}
-	for {
-		l, r = 0, size-1
-		h += 1
-		if max < h {
-			break
-		}
-		for l < r {
-			if height[l] < h {
-				l++
-			}
-			if height[r] < h {
-				r--
-			}
-			if height[l] >= h && height[r] >= h {
-				break
-			}
-		}
-		for i := l; i < r; i++ {
-			if height[i] < h {
-				res += 1
-			}
-		}
-	}
-	return res
+	return res - size*l
 }
