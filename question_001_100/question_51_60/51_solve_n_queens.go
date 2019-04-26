3954dae8caaf3d6e6020d0f5417a6eee70245df5
lupe <lupe.luo@klook.com>
2019-04-26 21:04:48 +0800

    51 N皇后

---
+++ question_001_100/question_51_60/51_solve_n_queens.go
@@ -0,0 +1,54 @@
+package question_51_60
+
+func solveNQueens(n int) [][]string {
+	flag := make([][]bool, n)
+	for i := range flag {
+		flag[i] = make([]bool, n)
+	}
+	return solveNQueensDfs(flag, 0, n)
+}
+
+func solveNQueensDfs(flag [][]bool, i, n int) (res [][]string) {
+	for j := 0; j < n; j++ {
+		flag[i][j] = true
+		if valid(flag, i, j, n) {
+			if i < n-1 {
+				res = append(res, solveNQueensDfs(flag, i+1, n)...)
+			} else {
+				res = append(res, parse(flag))
+			}
+		}
+		flag[i][j] = false
+	}
+	return
+}
+
+func parse(flag [][]bool) (res []string) {
+	for _, arr := range flag {
+		t := ""
+		for _, a := range arr {
+			if a {
+				t += "Q"
+			} else {
+				t += "."
+			}
+		}
+		res = append(res, t)
+	}
+	return res
+}
+
+func valid(flag [][]bool, i, j, n int) bool {
+	for k := 0; k < i; k++ {
+		if flag[k][j] {
+			return false
+		}
+		if j-1-k >= 0 && flag[i-1-k][j-1-k] {
+			return false
+		}
+		if j+1+k < n && flag[i-1-k][j+1+k] {
+			return false
+		}
+	}
+	return true
+}
---
+++ question_001_100/question_51_60/51_solve_n_queens_test.go
@@ -0,0 +1,26 @@
+package question_51_60
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_solveNQueens(t *testing.T) {
+	tests := []struct {
+		name string
+		n    int
+		want [][]string
+	}{
+		{"test#0", 1, [][]string{{"Q"}}},
+		{"test#1", 2, nil},
+		{"test#2", 3, nil},
+		{"test#3", 4, [][]string{{".Q..", "...Q", "Q...", "..Q."}, {"..Q.", "Q...", "...Q", ".Q.."}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solveNQueens(tt.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("solveNQueens() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
