4ddc7f19ed39e6103e827baf6b5700f874ba5e3f
lupe <lupe.luo@klook.com>
2019-05-13 20:23:12 +0800

    70 爬楼梯

--- question_001_100/question_61_70/70_climb_stairs.go
+++ question_001_100/question_61_70/70_climb_stairs.go
@@ -1,11 +1,22 @@
 package question_61_70
 
+var m = make(map[int]int)
+
 func climbStairs(n int) int {
 	if n == 1 {
 		return 1
 	} else if n == 2 {
 		return 2
 	} else {
-		return climbStairs(n-1) + climbStairs(n-2)
+		r1, ok := m[n-1]
+		if !ok {
+			r1 = climbStairs(n - 1)
+			m[n-1] = r1
+		}
+		r2, ok := m[n-2]
+		if !ok {
+			r2 = climbStairs(n - 2)
+		}
+		return r1 + r2
 	}
 }
