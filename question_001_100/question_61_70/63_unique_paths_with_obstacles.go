89cdc58c274becce24e5d4f5a57353b9184ab1f3
lupe <lupe.luo@klook.com>
2019-05-14 20:47:18 +0800

    63 不同路径 II添加缓存

--- question_001_100/question_61_70/63_unique_paths_with_obstacles.go
+++ question_001_100/question_61_70/63_unique_paths_with_obstacles.go
@@ -1,23 +1,32 @@
 package question_61_70
 
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m := make(map[[2]int]int)
 	r, rl, c, cl := 0, len(obstacleGrid)-1, 0, len(obstacleGrid[0])-1
 	if obstacleGrid[0][0] == 1 || obstacleGrid[rl][cl] == 1 {
 		return 0
 	}
-	return uniquePathsWithObstaclesDfs(r, rl, c, cl, obstacleGrid)
+	return uniquePathsWithObstaclesDfs(r, rl, c, cl, obstacleGrid, m)
 }
 
-func uniquePathsWithObstaclesDfs(r, rl, c, cl int, obstacleGrid [][]int) int {
-	if r == rl && c == cl {
+func uniquePathsWithObstaclesDfs(r, rl, c, cl int, obstacleGrid [][]int, m map[[2]int]int) int {
+	if r == rl && c == cl && obstacleGrid[rl][cl] != 1 {
 		return 1
 	}
-	res1, res2 := 0, 0
+	res1, res2, ok := 0, 0, false
 	if r < rl && obstacleGrid[r+1][c] != 1 {
-		res1 = uniquePathsWithObstaclesDfs(r+1, rl, c, cl, obstacleGrid)
+		res1, ok = m[[2]int{r + 1, c}]
+		if !ok {
+			res1 = uniquePathsWithObstaclesDfs(r+1, rl, c, cl, obstacleGrid, m)
+			m[[2]int{r + 1, c}] = res1
+		}
 	}
 	if c < cl && obstacleGrid[r][c+1] != 1 {
-		res2 = uniquePathsWithObstaclesDfs(r, rl, c+1, cl, obstacleGrid)
+		res2, ok = m[[2]int{r, c + 1}]
+		if !ok {
+			res2 = uniquePathsWithObstaclesDfs(r, rl, c+1, cl, obstacleGrid, m)
+			m[[2]int{r, c + 1}] = res2
+		}
 	}
 	return res1 + res2
 }
