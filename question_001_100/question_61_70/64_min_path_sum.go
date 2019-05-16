aaf8a9e7f8b99de79649c1bc296a9cda3353251c
lupe <lupe.luo@klook.com>
2019-05-16 22:10:48 +0800

    64 最小路径和优化

--- question_001_100/question_61_70/64_min_path_sum.go
+++ question_001_100/question_61_70/64_min_path_sum.go
@@ -1,34 +1,21 @@
 package question_61_70
 
 func minPathSum(grid [][]int) int {
-	r, rl, c, cl := 0, len(grid)-1, 0, len(grid[0])-1
-	var m = make(map[[2]int]int)
-	return minPathSumDfs(r, rl, c, cl, grid, m)
-}
-
-func minPathSumDfs(r, rl, c, cl int, grid [][]int, m map[[2]int]int) int {
-	res := -1
-	res1, res2, ok := 0, 0, false
-	if r < rl {
-		res1, ok = m[[2]int{r + 1, c}]
-		if !ok {
-			res1 = minPathSumDfs(r+1, rl, c, cl, grid, m)
-			m[[2]int{r + 1, c}] = res1
-		}
-		res = res1
+	r, rl, c, cl, min := 1, len(grid), 1, len(grid[0]), 0
+	for r = 1; r < rl; r++ {
+		grid[r][0] += grid[r-1][0]
 	}
-	if c < cl {
-		res2, ok = m[[2]int{r, c + 1}]
-		if !ok {
-			res2 = minPathSumDfs(r, rl, c+1, cl, grid, m)
-			m[[2]int{r, c + 1}] = res2
-		}
-		if res == -1 || res > res2 {
-			res = res2
+	for c = 1; c < cl; c++ {
+		grid[0][c] += grid[0][c-1]
+	}
+	for r = 1; r < rl; r++ {
+		for c = 1; c < cl; c++ {
+			min = grid[r][c-1]
+			if min > grid[r-1][c] {
+				min = grid[r-1][c]
+			}
+			grid[r][c] += min
 		}
 	}
-	if res == -1 {
-		res = 0
-	}
-	return grid[r][c] + res
+	return grid[rl-1][cl-1]
 }
