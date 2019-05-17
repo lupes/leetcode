40d868c22f3bd4cf74d7cf857d061bd266594461
lupe <lupe.luo@klook.com>
2019-05-17 23:37:53 +0800

    78 子集

--- question_001_100/question_41_50/42_trap.go
+++ question_001_100/question_41_50/42_trap.go
@@ -1,5 +1,7 @@
 package question_41_50
 
+import "fmt"
+
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -11,10 +13,12 @@ func max(a, b int) int {
 func trap(height []int) int {
 	size := len(height)
 	var l, r, res int
+	fmt.Printf("%v\n", height)
 	for i := 0; i < size; i++ {
 		l = max(l, height[i])
 		r = max(r, height[size-1-i])
 		res += l + r - height[i]
+		fmt.Printf("%d %d %d %d\n", i, l, r, res)
 	}
 	return res - size*l
 }
---
+++ question_001_100/question_71_80/78_subsets.go
@@ -0,0 +1,39 @@
+package question_71_80
+
+func subsets(nums []int) [][]int {
+	var l = len(nums)
+	var flag = make([]bool, l)
+	var res [][]int
+	for n := 0; n <= l/2; n++ {
+		res = append(res, subsetsDfs(nums, flag, n, 0, l)...)
+	}
+	return res
+}
+
+func subsetsDfs(nums []int, flag []bool, n, i, l int) (res [][]int) {
+	if n == 0 {
+		return get(nums, flag)
+	}
+	last := l
+	if l%2 == 0 && n == l/2 {
+		last = 1
+	}
+	for j := i; j < last; j++ {
+		flag[j] = true
+		res = append(res, subsetsDfs(nums, flag, n-1, j+1, l)...)
+		flag[j] = false
+	}
+	return
+}
+
+func get(nums []int, flag []bool) [][]int {
+	var res = make([][]int, 2)
+	for i, f := range flag {
+		if f {
+			res[0] = append(res[0], nums[i])
+		} else {
+			res[1] = append(res[1], nums[i])
+		}
+	}
+	return res
+}
