c68e9a8444238f5559e898ba4b6c0e48fc3879fd
lupe <lupe.luo@klook.com>
2019-05-20 19:30:50 +0800

    79 单词搜索 5.19忘记提交...

---
+++ question_001_100/question_71_80/79_exist.go
@@ -0,0 +1,38 @@
+package question_71_80
+
+func exist(board [][]byte, word string) bool {
+	for r, row := range board {
+		for c, b := range row {
+			if b == word[0] {
+				if existDfs(board, r, c, len(board), len(row), 1, word) {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func existDfs(board [][]byte, r, c, lr, lc, i int, word string) (e bool) {
+	if i == len(word) {
+		return true
+	}
+	var t byte = 0
+	t, board[r][c] = board[r][c], t
+	defer func() {
+		t, board[r][c] = board[r][c], t
+	}()
+	if (r+1 < lr) && (board[r+1][c] == word[i]) {
+		e = e || existDfs(board, r+1, c, lr, lc, i+1, word)
+	}
+	if !e && (r-1 >= 0) && (board[r-1][c] == word[i]) {
+		e = e || existDfs(board, r-1, c, lr, lc, i+1, word)
+	}
+	if !e && (c+1 < lc) && (board[r][c+1] == word[i]) {
+		e = e || existDfs(board, r, c+1, lr, lc, i+1, word)
+	}
+	if !e && (c-1 >= 0) && (board[r][c-1] == word[i]) {
+		e = e || existDfs(board, r, c-1, lr, lc, i+1, word)
+	}
+	return e
+}
