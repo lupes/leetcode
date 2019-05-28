bf89d9eb1d583bf5807225e1b7993cf110bd63f2
lupe <lupe.luo@klook.com>
2019-05-28 19:51:13 +0800

    162. 寻找峰值

--- question_101_200/question_161_170/162_find_peek_element.go
+++ question_101_200/question_161_170/162_find_peek_element.go
@@ -1,10 +1,16 @@
 package question_161_170
 
 func findPeakElement(nums []int) int {
-	size := len(nums)
-	for i, _ := range nums {
-		if (size == 1) || (i < size-1 && nums[i] > nums[i+1]) || (i == size-1) {
-			return i
+	l, r := 0, len(nums)
+	for r > l {
+		c := (l + r) / 2
+		if c == l {
+			return c
+		}
+		if nums[c] < nums[c-1] {
+			r = c
+		} else {
+			l = c
 		}
 	}
 	return -1
--- question_101_200/question_161_170/162_find_peek_element_test.go
+++ question_101_200/question_161_170/162_find_peek_element_test.go
@@ -1,6 +1,8 @@
 package question_161_170
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_findPeakElement(t *testing.T) {
 	tests := []struct {
@@ -14,7 +16,7 @@ func Test_findPeakElement(t *testing.T) {
 		{"test", []int{0, 1}, 1},
 		{"test", []int{0, 1, 0}, 1},
 		{"test", []int{1, 2, 3, 1}, 2},
-		{"test", []int{1, 2, 1, 3, 5, 6, 4}, 1},
+		{"test", []int{1, 2, 1, 3, 5, 6, 4}, 5},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
