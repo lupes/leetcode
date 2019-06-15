25f551f9da83aa86a5ef6fbffc7a345e63443309
lupe <lupe.luo@klook.com>
2019-06-15 22:32:24 +0800

    111. 二叉树的最小深度

---
+++ question_101_200/question_111_120/111_min_depth.go
@@ -0,0 +1,29 @@
+package question_111_120
+
+// 111. 二叉树的最小深度
+// https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftDepth := minDepth(root.Left)
+	rightDepth := minDepth(root.Right)
+	if leftDepth == 0 {
+		return rightDepth + 1
+	}
+	if rightDepth == 0 {
+		return leftDepth + 1
+	}
+	min := leftDepth
+	if rightDepth < leftDepth {
+		min = rightDepth
+	}
+	return min + 1
+}
