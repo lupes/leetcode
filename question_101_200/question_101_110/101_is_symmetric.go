bc67dcb4c6c6d94757a9d13cc4285a2f32d03945
lupe <lupe.luo@klook.com>
2019-06-17 20:20:36 +0800

    101. 对称二叉树

---
+++ question_101_200/question_101_110/101_is_symmetric.go
@@ -0,0 +1,28 @@
+package question_101_110
+
+// 101. 对称二叉树
+// https://leetcode-cn.com/problems/symmetric-tree/
+
+func isSymmetric(root *TreeNode) bool {
+	var next = []*TreeNode{root}
+	for len(next) != 0 {
+		var son []*TreeNode
+		for _, t := range next {
+			if t != nil {
+				son = append(son, t.Left, t.Right)
+			}
+		}
+		if len(son)%2 != 0 {
+			return false
+		}
+		for i, l := 0, len(son); i < l/2; i++ {
+			if (son[i] != nil && son[l-i-1] == nil) ||
+				(son[i] == nil && son[l-i-1] != nil) ||
+				(son[i] != nil && son[l-i-1] != nil && son[i].Val != son[l-i-1].Val) {
+				return false
+			}
+		}
+		next = son
+	}
+	return true
+}
---
+++ question_101_200/question_101_110/101_is_symmetric_test.go
@@ -0,0 +1,24 @@
+package question_101_110
+
+import "testing"
+
+func Test_isSymmetric(t *testing.T) {
+	tests := []struct {
+		name string
+		root *TreeNode
+		want bool
+	}{
+		{"test", nil, true},
+		{"test", &TreeNode{Val: 1}, true},
+		{"test", &TreeNode{Val: 1, Left: &TreeNode{Val: 2}}, false},
+		{"test", &TreeNode{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 2}}, true},
+		{"test", &TreeNode{Val: 1, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 3}}, Right: &TreeNode{Val: 2, Right: &TreeNode{Val: 3}}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSymmetric(tt.root); got != tt.want {
+				t.Errorf("isSymmetric() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
