4cd320a28bd4c10e3965b1967694373ae6ae7ddb
lupe <lupe.luo@klook.com>
2019-06-29 13:24:59 +0800

    219. 存在重复元素 II

---
+++ question_201_300/question_211_220/219_contains_nearby_duplicate.go
@@ -0,0 +1,15 @@
+package question_211_220
+
+// 219. 存在重复元素 II
+// https://leetcode-cn.com/problems/contains-duplicate-ii/
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	for i := 0; i < len(nums); i++ {
+		for j := 1; j <= k && i+j < len(nums); j++ {
+			if nums[i] == nums[i+j] {
+				return true
+			}
+		}
+	}
+	return false
+}
