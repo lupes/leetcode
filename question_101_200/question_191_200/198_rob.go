a4aea8aa8902d379d6aca34d3857b5a234729684
lupe <lupe.luo@klook.com>
2019-07-07 20:30:06 +0800

    198. 打家劫舍

--- question_101_200/question_191_200/198_rob.go
+++ question_101_200/question_191_200/198_rob.go
@@ -4,33 +4,13 @@ package question_191_200
 // https://leetcode-cn.com/problems/house-robber/
 
 func rob(nums []int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-	var data = make(map[int]int)
-	return robDfs(data, nums)
-}
-
-func robDfs(data map[int]int, nums []int) int {
-	l := len(nums)
-	if r, ok := data[l]; ok {
-		return r
-	}
-	var res int
-	if l == 1 {
-		res = nums[0]
-	} else if l == 2 {
-		res = nums[0]
-		if nums[1] > nums[0] {
-			res = nums[1]
+	var last, now, tmp = 0, 0, 0
+	for _, n := range nums {
+		tmp = now
+		if last+n > now {
+			now = last + n
 		}
-	} else {
-		res = rob(nums[1:])
-		s2 := nums[0] + rob(nums[2:])
-		if s2 > res {
-			res = s2
-		}
+		last = tmp
 	}
-	data[l] = res
-	return res
+	return now
 }
