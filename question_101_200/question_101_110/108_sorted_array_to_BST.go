82fc7baa69037ca05219c8b993a8c8ae213b908a
lupe <lupe.luo@klook.com>
2019-07-10 21:48:33 +0800

    108. 将有序数组转换为二叉搜索树

---
+++ question_101_200/question_101_110/108_sorted_array_to_BST.go
@@ -0,0 +1,14 @@
+package question_101_110
+
+// 108. 将有序数组转换为二叉搜索树
+// https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	l := len(nums)
+	if l == 0 {
+		return nil
+	}
+	left := sortedArrayToBST(nums[:l/2])
+	right := sortedArrayToBST(nums[l/2+1:])
+	return &TreeNode{Val: nums[l/2], Left: left, Right: right}
+}
---
+++ question_101_200/question_101_110/108_sorted_array_to_BST_test.go
@@ -0,0 +1,26 @@
+package question_101_110
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_sortedArrayToBST(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want *TreeNode
+	}{
+		{"test", nil, nil},
+		{"test", []int{1}, &TreeNode{Val: 1}},
+		{"test", []int{1, 2}, &TreeNode{Val: 2, Left: &TreeNode{Val: 1}}},
+		{"test", []int{1, 2, 3}, &TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 3}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedArrayToBST(tt.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedArrayToBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
