77e4077a993e3b732a90bd77f47f79bb31559409
lupe <lupe.luo@klook.com>
2019-07-21 21:11:55 +0800

    148. 排序链表

---
+++ question_101_200/question_141_150/148_sort_list.go
@@ -0,0 +1,27 @@
+package question_141_150
+
+import "sort"
+
+// 148. 排序链表
+// https://leetcode-cn.com/problems/sort-list/
+
+func sortList(head *ListNode) *ListNode {
+	var arr []*ListNode
+	for n := head; n != nil; n = n.Next {
+		arr = append(arr, n)
+	}
+	sort.Slice(arr, func(i, j int) bool {
+		if arr[i].Val > arr[j].Val {
+			return false
+		}
+		return true
+	})
+	var res = &ListNode{}
+	next := res
+	for _, n := range arr {
+		next.Next = n
+		next = n
+		next.Next = nil
+	}
+	return res.Next
+}
