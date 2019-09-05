bb4dcb769690d7e9718d4bd415a6c7a9a0166b63
lupe <lupe.luo@klook.com>
2019-09-05 22:05:03 +0800

    397. 整数替换

--- question_301_400/question_391_400/397_integer_replacement.go
+++ question_301_400/question_391_400/397_integer_replacement.go
@@ -3,20 +3,19 @@ package question_391_400
 // 397. 整数替换
 // https://leetcode-cn.com/problems/integer-replacement/
 
-var m = map[int]int{1: 0}
-
 func integerReplacement(n int) int {
-	if res, ok := m[n]; ok {
-		return res
-	}
-	res, t := 0, 0
-	if n%2 == 0 {
-		res = integerReplacement(n / 2)
-	} else {
-		res, t = integerReplacement(n+1), integerReplacement(n-1)
-		if res > t {
-			res = t
+	res := 0
+	for ; n > 1; res++ {
+		if n == 3 {
+			n++
+		}
+		if n%2 == 0 {
+			n /= 2
+		} else if ((n+1)/2)%2 == 0 {
+			n += 1
+		} else {
+			n -= 1
 		}
 	}
-	return res + 1
+	return res
 }
