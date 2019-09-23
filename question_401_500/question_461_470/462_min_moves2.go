ed264d3f512cb627d5d7f84a9cff240f5069e441
lupe <lupe.luo@klook.com>
2019-09-23 09:53:28 +0800

    462. 最少移动次数使数组元素相等 II

--- question_401_500/question_461_470/462_min_moves2.go
+++ question_401_500/question_461_470/462_min_moves2.go
@@ -1,20 +1,13 @@
 package question_461_470
 
+import "sort"
+
 // 462. 最少移动次数使数组元素相等 II
 // https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements-ii/
 
 func minMoves2(nums []int) int {
 	var l, r = len(nums), 0
-	for i := 0; i < l-1; i++ {
-		m, t := nums[0], 0
-		for j := 0; j < l-i; j++ {
-			if nums[j] > m {
-				t = j
-				m = nums[j]
-			}
-		}
-		nums[t], nums[l-i-1] = nums[l-i-1], nums[t]
-	}
+	sort.Ints(nums)
 	for i, j := 0, l-1; j > i; i, j = i+1, j-1 {
 		r += nums[j] - nums[i]
 	}
