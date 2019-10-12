8c6625b03067a5b140cb23eed9ebd2701421d27f
lupe <lupe.luo@klook.com>
2019-10-12 09:36:01 +0800

    503. 下一个更大元素 II

---
+++ question_501_600/question_501_510/503_next_greater_element.go
@@ -0,0 +1,20 @@
+package question_491_500
+
+// 503. 下一个更大元素 II
+// https://leetcode-cn.com/problems/next-greater-element-ii/
+
+func nextGreaterElements(nums []int) []int {
+	var res, l, flag = make([]int, len(nums)), len(nums), false
+	for i, n := range nums {
+		res[i], flag = -1, false
+		for j := l - 1; j >= 0; j-- {
+			if nums[j] > n {
+				res[i], flag = nums[j], true
+			}
+			if i == j && flag {
+				break
+			}
+		}
+	}
+	return res
+}
---
+++ question_501_600/question_501_510/503_next_greater_element_test.go
@@ -0,0 +1,27 @@
+package question_491_500
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_nextGreaterElement(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want []int
+	}{
+		{"test", []int{4, 1, 2}, []int{-1, 2, 4}},
+		{"test", []int{2, 4}, []int{4, -1}},
+		{"test", []int{1, 2, 1}, []int{2, -1, 2}},
+		{"test", []int{2, 1, 3, 6, 5, 4, 2, 1, 0}, []int{3, 3, 6, -1, 6, 6, 3, 2, 2}},
+		{"test", []int{1, 8, -1, -100, -1, 222, 1111111, -111111}, []int{8, 222, 222, -1, 222, 1111111, -1, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nextGreaterElements(tt.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("nextGreaterElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
