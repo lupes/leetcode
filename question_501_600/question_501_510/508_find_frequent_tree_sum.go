b8adc485b47f99eda604010b629c3baf69bd2666
lupe <lupe.luo@klook.com>
2019-10-14 09:49:59 +0800

    508. 出现次数最多的子树元素和

---
+++ question_501_600/question_501_510/508_find_frequent_tree_sum.go
@@ -0,0 +1,40 @@
+package question_491_500
+
+// 508. 出现次数最多的子树元素和
+// https://leetcode-cn.com/problems/most-frequent-subtree-sum/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findFrequentTreeSum(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	var tmp = make(map[int]int)
+	var res = make(map[int][]int)
+	var max = 0
+	findFrequentTreeSumDfs(root, tmp)
+
+	for k, v := range tmp {
+		if v > max {
+			max = v
+		}
+		res[v] = append(res[v], k)
+	}
+	return res[max]
+}
+
+func findFrequentTreeSumDfs(root *TreeNode, res map[int]int) (sum int) {
+	sum = root.Val
+	if root.Left != nil {
+		sum += findFrequentTreeSumDfs(root.Left, res)
+	}
+	if root.Right != nil {
+		sum += findFrequentTreeSumDfs(root.Right, res)
+	}
+	res[sum]++
+	return sum
+}
---
+++ question_501_600/question_501_510/508_find_frequent_tree_sum_test.go
@@ -0,0 +1,28 @@
+package question_491_500
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
+
+func Test_findFrequentTreeSum(t *testing.T) {
+	tests := []struct {
+		name string
+		root *TreeNode
+		want []int
+	}{
+		{"test", &TreeNode{Val: 5, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: -3}}, []int{-3, 2, 4}},
+		{"test", &TreeNode{Val: 5, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: -5}}, []int{2}},
+		{"test", &TreeNode{Val: 5, Left: &TreeNode{Val: 2}}, []int{2, 7}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := findFrequentTreeSum(tt.root)
+			sort.Ints(got)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findFrequentTreeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
