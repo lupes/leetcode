4f9ed77e977e3cc4e036b9febed5566e2f5f2ca3
lupe <lupe.luo@klook.com>
2019-10-19 11:45:21 +0800

    506. 相对名次

---
+++ question_501_600/question_501_510/506_find_relative_ranks.go
@@ -0,0 +1,36 @@
+package question_491_500
+
+import (
+	"sort"
+	"strconv"
+)
+
+// 506. 相对名次
+// https://leetcode-cn.com/problems/relative-ranks/
+
+func findRelativeRanks(nums []int) []string {
+	var tmp = make([]int, len(nums))
+	copy(tmp, nums)
+	sort.Slice(tmp, func(i, j int) bool {
+		return tmp[i] > tmp[j]
+	})
+	var res []string
+	for _, n := range nums {
+		for i, t := range tmp {
+			if t == n {
+				switch i {
+				case 0:
+					res = append(res, "Gold Medal")
+				case 1:
+					res = append(res, "Silver Medal")
+				case 2:
+					res = append(res, "Bronze Medal")
+				default:
+					res = append(res, strconv.Itoa(i+1))
+				}
+				break
+			}
+		}
+	}
+	return res
+}
---
+++ question_501_600/question_501_510/506_find_relative_ranks_test.go
@@ -0,0 +1,22 @@
+package question_491_500
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findRelativeRanks(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want []string
+	}{
+		{[]int{5, 4, 3, 2, 1}, []string{"Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"}},
+	}
+	for _, tt := range tests {
+		t.Run("test", func(t *testing.T) {
+			if got := findRelativeRanks(tt.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findRelativeRanks() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
