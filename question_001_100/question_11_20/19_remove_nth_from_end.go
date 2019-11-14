ae83c65c5235087bb700b3c91d81693bfb1e7507
lupe <lupe.luo@klook.com>
2019-11-14 16:44:36 +0800

    19. 删除链表的倒数第N个节点

--- question_001_100/question_11_20/19_remove_nth_from_end.go
+++ question_001_100/question_11_20/19_remove_nth_from_end.go
@@ -1,33 +1,34 @@
 package question_11_20
 
+import "fmt"
+
+// 19. 删除链表的倒数第N个节点
+// https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func (self ListNode) String() string {
+	return fmt.Sprintf("{Val:%d Next:%s}", self.Val, self.Next)
 }
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	if head == nil {
 		return nil
 	}
-	var nodes []*ListNode
-	var tmp = head
-	for tmp != nil {
-		nodes = append(nodes, tmp)
-		tmp = tmp.Next
+	head = &ListNode{Next: head}
+	num := 0
+	var next = head
+	for next.Next != nil {
+		num++
+		next = next.Next
 	}
-	size := len(nodes)
-	if size < n {
-		return nil
-	} else if n == 1 {
-		if size == 1 {
-			return nil
-		}
-		nodes[size-2].Next = nil
-	} else if size == n {
-		head = head.Next
-	} else {
-		index := size - n
-		nodes[index-1].Next = nodes[index+1]
+	next = head
+	for i := num - n; i > 0; i-- {
+		next = next.Next
 	}
-	return head
+	next.Next = next.Next.Next
+	return head.Next
 }
--- question_001_100/question_11_20/19_remove_nth_from_end_test.go
+++ question_001_100/question_11_20/19_remove_nth_from_end_test.go
@@ -14,23 +14,19 @@ var head4 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Ne
 var res4 = &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}
 
 func Test_removeNthFromEnd(t *testing.T) {
-	type args struct {
+	tests := []struct {
 		head *ListNode
 		n    int
-	}
-	tests := []struct {
-		name string
-		args args
 		want *ListNode
 	}{
-		{"test#1", args{head1, 1}, nil},
-		{"test#2", args{head2, 2}, nil},
-		{"test#3", args{head3, 2}, res3},
-		{"test#4", args{head4, 5}, res4},
+		{head1, 1, nil},
+		{head2, 2, nil},
+		{head3, 2, res3},
+		{head4, 5, res4},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := removeNthFromEnd(tt.args.head, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+		t.Run("test", func(t *testing.T) {
+			if got := removeNthFromEnd(tt.head, tt.n); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("removeNthFromEnd() = %v, want %v", got, tt.want)
 			}
 		})
