20065671acae9336a37ae62d0b3119987f132e45
lupe <lupe.luo@klook.com>
2019-11-14 09:16:46 +0800

    53. 最大子序和

--- question_001_100/question_51_60/53_max_sub_array.go
+++ question_001_100/question_51_60/53_max_sub_array.go
@@ -1,19 +1,20 @@
 package question_51_60
 
+// 53. 最大子序和
+// https://leetcode-cn.com/problems/maximum-subarray/
+
 func maxSubArray(nums []int) int {
-	res := nums[0]
-	for i, n := range nums {
-		max := n
-		t := n
-		for _, m := range nums[i+1:] {
-			t = t + m
-			if t > max {
-				max = t
-			}
+	var dp = make([]int, len(nums))
+	dp[0] = nums[0]
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i] = dp[i-1] + nums[i]
+		if dp[i] < nums[i] {
+			dp[i] = nums[i]
 		}
-		if max > res {
-			res = max
+		if dp[i] > max {
+			max = dp[i]
 		}
 	}
-	return res
+	return max
 }
