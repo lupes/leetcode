1e6bc9ed9ad8015c80257d4b6f647fcf9552bfc8
lupe <lupe.luo@klook.com>
2019-11-18 09:27:12 +0800

    509. 斐波那契数

--- question_501_600/question_501_510/509_fibonacci_number.go
+++ question_501_600/question_501_510/509_fibonacci_number.go
@@ -2,14 +2,13 @@ package question_501_510
 
 // 509. 斐波那契数
 // https://leetcode-cn.com/problems/fibonacci-number
-// Topics: 数组
+// Topics: 数组 动态规划
 
 func fib(N int) int {
-	if N == 0 {
-		return 0
-	} else if N == 1 || N == 2 {
-		return 1
-	} else {
-		return fib(N-1) + fib(N-2)
+	var fibs = make([]int, N+2)
+	fibs[1] = 1
+	for i := 2; i <= N; i++ {
+		fibs[i] = fibs[i-1] + fibs[i-2]
 	}
+	return fibs[N]
 }
