73f75d921c9d38a45b2552b90a23f74cdef3dcf1
lupe <lupe.luo@klook.com>
2019-11-25 09:25:31 +0800

    628. 三个数的最大乘积

--- question_601_700/question_621_630/628_maximum_product_of_three_numbers.go
+++ question_601_700/question_621_630/628_maximum_product_of_three_numbers.go
@@ -1,9 +1,20 @@
 package question_621_630
 
+import (
+	"sort"
+)
+
 // 628. 三个数的最大乘积
 // https://leetcode-cn.com/problems/maximum-product-of-three-numbers
 // Topics: 数组 数学
 
 func maximumProduct(nums []int) int {
-	return 0
+	sort.Ints(nums)
+	l := len(nums)
+	res1 := nums[l-1] * nums[l-2] * nums[l-3]
+	res2 := nums[l-1] * nums[0] * nums[1]
+	if res1 < res2 {
+		res1 = res2
+	}
+	return res1
 }
