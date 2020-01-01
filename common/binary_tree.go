30619d1416677bd27a1ee9273ac11ecc648e4f27
lupe <lupe.luo@klook.com>
2020-01-01 19:37:04 +0800

    优化

--- common/binary_tree.go
+++ common/binary_tree.go
@@ -10,11 +10,11 @@ type TreeNode struct {
 
 func (self *TreeNode) String() string {
 	if self.Left != nil && self.Right != nil {
-		return fmt.Sprintf("&TreeNode{Val:%d, Left:%+v, Right:%+v},", self.Val, self.Left, self.Right)
+		return fmt.Sprintf("&TreeNode{Val:%d Left:%+v Right:%+v}", self.Val, self.Left, self.Right)
 	} else if self.Left != nil {
-		return fmt.Sprintf("&TreeNode{Val:%d, Left:%+v},", self.Val, self.Left)
+		return fmt.Sprintf("&TreeNode{Val:%d Left:%+v}", self.Val, self.Left)
 	} else if self.Right != nil {
-		return fmt.Sprintf("&TreeNode{Val:%d, Right:%+v},", self.Val, self.Right)
+		return fmt.Sprintf("&TreeNode{Val:%d Right:%+v}", self.Val, self.Right)
 	} else {
 		return fmt.Sprintf("&TreeNode{Val:%d}", self.Val)
 	}
