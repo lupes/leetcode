3653ceadaa5b764f5611aab95c1fcde8e6283b15
lupe <lupe.luo@klook.com>
2020-01-01 19:36:32 +0800

    652. 寻找重复的子树

--- question_601_700/question_651_660/652_find_duplicate_subtrees.go
+++ question_601_700/question_651_660/652_find_duplicate_subtrees.go
@@ -1,6 +1,8 @@
 package question_651_660
 
 import (
+	"encoding/json"
+
 	. "github.com/lupes/leetcode/common"
 )
 
@@ -9,5 +11,27 @@ import (
 // Topics: 树
 
 func findDuplicateSubtrees(root *TreeNode) []*TreeNode {
-	return nil
+	var flag = make(map[string]*TreeNode)
+	var resFlag = make(map[*TreeNode]struct{})
+	var res = make([]*TreeNode, 0)
+	findDuplicateSubtreesHelper(root, flag, resFlag, &res)
+	return res
+}
+
+func findDuplicateSubtreesHelper(root *TreeNode, flag map[string]*TreeNode, resFlag map[*TreeNode]struct{}, res *[]*TreeNode) {
+	if root == nil {
+		return
+	}
+	buffer, _ := json.Marshal(root)
+	node, ok := flag[string(buffer)]
+	if ok {
+		if _, ok := resFlag[node]; !ok {
+			resFlag[node] = struct{}{}
+			*res = append(*res, node)
+		}
+	} else {
+		flag[string(buffer)] = root
+	}
+	findDuplicateSubtreesHelper(root.Left, flag, resFlag, res)
+	findDuplicateSubtreesHelper(root.Right, flag, resFlag, res)
 }
---
+++ question_601_700/question_651_660/652_find_duplicate_subtrees_test.go
@@ -0,0 +1,52 @@
+package question_651_660
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/lupes/leetcode/common"
+)
+
+//     0
+//   0   0
+//  0 n  n 0
+// n n    0 n
+
+func Test_findDuplicateSubtrees(t *testing.T) {
+	tests := []struct {
+		root *TreeNode
+		want []*TreeNode
+	}{
+		{
+			NewNode(1, NewNode(2, NewNode(4)), NewNode(3, NewNode(2, NewNode(4)), NewNode(4))),
+			[]*TreeNode{NewNode(4), NewNode(2, NewNode(4))},
+		},
+		{
+			NewNode(2, NewNode(2, NewNode(3)), NewNode(2, NewNode(3))),
+			[]*TreeNode{NewNode(2, NewNode(3)), NewNode(3)},
+		},
+		{
+			NewNode(0, NewNode(0, NewNode(0)), NewNode(0, nil, NewNode(0, NewNode(0), NewNode(0)))),
+			[]*TreeNode{NewNode(0)},
+		},
+		{
+			NewNode(0, NewNode(0, NewNode(0)), NewNode(0, nil, NewNode(0, NewNode(0), NewNode(0)))),
+			[]*TreeNode{NewNode(0)},
+		},
+		{
+			NewNode(0, NewNode(0, NewNode(0, NewNode(0), NewNode(0))), NewNode(0, nil, NewNode(0, NewNode(0), NewNode(0)))),
+			[]*TreeNode{NewNode(0), NewNode(0, NewNode(0), NewNode(0))},
+		},
+		{
+			NewNode(0, NewNode(0, NewNode(0)), NewNode(0, nil, NewNode(0, NewNode(0)))),
+			[]*TreeNode{NewNode(0, NewNode(0)), NewNode(0)},
+		},
+	}
+	for _, tt := range tests {
+		t.Run("test", func(t *testing.T) {
+			if got := findDuplicateSubtrees(tt.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findDuplicateSubtrees() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
