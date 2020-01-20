9b8c7e573bdfc35ad4551249a79083652af9abdf
lupe <lupe.luo@klook.com>
2020-01-20 11:23:34 +0800

    707. 设计链表

--- question_701_800/question_701_710/707_design_linked_list.go
+++ question_701_800/question_701_710/707_design_linked_list.go
@@ -1,168 +1,114 @@
 package question_701_710
 
+import (
+	"fmt"
+)
+
 // 707. 设计链表
 // https://leetcode-cn.com/problems/design-linked-list
 // Topics: 设计 链表
 
+func (this MyLinkedList) String() string {
+	return fmt.Sprintf("%d %s", this.Val, this.Next)
+}
+
 type MyLinkedList struct {
-	arr []int
+	Val  int
+	Next *MyLinkedList
+	Prev *MyLinkedList
 }
 
+/** Initialize your data structure here. */
 func Constructor5() MyLinkedList {
-	return MyLinkedList{arr: make([]int, 0)}
+	return MyLinkedList{Val: 0}
 }
 
+/** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
 func (this *MyLinkedList) Get(index int) int {
-	if len(this.arr) > index && index >= 0 {
-		return this.arr[index]
+	node := this.findIndex(index)
+	if node == nil {
+		return -1
 	}
-	return -1
+	return node.Val
 }
 
+func (this *MyLinkedList) addFirst(val int) {
+	now := &MyLinkedList{Val: val}
+	this.Next = now
+	this.Prev = now
+}
+
+func (this *MyLinkedList) findIndex(index int) *MyLinkedList {
+	var next = this.Next
+	for i := 0; i < index && next != nil; i++ {
+		next = next.Next
+	}
+	return next
+}
+
+/** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
 func (this *MyLinkedList) AddAtHead(val int) {
-	this.arr = append([]int{val}, this.arr...)
+	if this.Val == 0 {
+		this.addFirst(val)
+	} else {
+		now := &MyLinkedList{Val: val}
+		now.Next = this.Next
+		this.Next.Prev = now
+		this.Next = now
+	}
+	this.Val++
 }
 
 /** Append a node of value val to the last element of the linked list. */
 func (this *MyLinkedList) AddAtTail(val int) {
-	this.arr = append(this.arr, val)
+	if this.Val == 0 {
+		this.addFirst(val)
+	} else {
+		now := &MyLinkedList{Val: val}
+		now.Prev = this.Prev
+		now.Prev.Next = now
+		this.Prev = now
+	}
+	this.Val++
 }
 
-/** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
 func (this *MyLinkedList) AddAtIndex(index int, val int) {
-	if index == 0 {
+	if index <= 0 {
 		this.AddAtHead(val)
-	} else if index == len(this.arr) {
+		return
+	} else if index == this.Val {
 		this.AddAtTail(val)
-	} else if len(this.arr) > index && index >= 0 {
-		this.arr = append(this.arr[:index], append([]int{val}, this.arr[index:]...)...)
+		return
+	} else if index > this.Val {
+		return
 	}
+	node := this.findIndex(index)
+	now := &MyLinkedList{Val: val}
+	now.Next = node
+	now.Prev = node.Prev
+	node.Prev.Next = now
+	node.Prev = now
+	this.Val++
 }
 
 /** Delete the index-th node in the linked list, if the index is valid. */
 func (this *MyLinkedList) DeleteAtIndex(index int) {
-	if index == 0 {
-		this.arr = this.arr[1:]
-	} else if index == len(this.arr)-1 {
-		this.arr = this.arr[:len(this.arr)-1]
-	} else if len(this.arr) > index && index >= 0 {
-		this.arr = append(this.arr[:index], this.arr[index+1:]...)
+	if index < 0 || index >= this.Val || this.Val == 0 {
+		return
 	}
+	if this.Val == 1 {
+		this.Next = nil
+		this.Prev = nil
+	} else if index == 0 {
+		this.Next = this.Next.Next
+		this.Next.Prev = nil
+	} else if this.Val == index+1 {
+		this.Prev = this.Prev.Prev
+		this.Prev.Next = nil
+	} else {
+		node := this.findIndex(index)
+		node.Prev.Next = node.Next
+		node.Next.Prev = node.Prev
+	}
+	this.Val--
 }
-
-//func (this MyLinkedList) String() string {
-//	return fmt.Sprintf("%d %s", this.Val, this.Next)
-//}
-//
-//type MyLinkedList struct {
-//	Val  int
-//	Next *MyLinkedList
-//	Prev *MyLinkedList
-//}
-//
-///** Initialize your data structure here. */
-//func Constructor5() MyLinkedList {
-//	this := MyLinkedList{
-//		Val: 0,
-//	}
-//	return this
-//}
-//
-///** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
-//func (this *MyLinkedList) Get(index int) int {
-//	node := this.findIndex(index)
-//	if node == nil {
-//		return -1
-//	}
-//	return node.Val
-//}
-//
-//func (this *MyLinkedList) addFirst(val int) {
-//	now := &MyLinkedList{Val: val}
-//	this.Next = now
-//	this.Prev = now
-//}
-//
-//func (this *MyLinkedList) findIndex(index int) *MyLinkedList {
-//	var next = this.Next
-//	for i := 0; i < index && next != nil; i++ {
-//		next = next.Next
-//	}
-//	return next
-//}
-//
-///** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
-//func (this *MyLinkedList) AddAtHead(val int) {
-//	if this.Val == 0 {
-//		this.addFirst(val)
-//	} else {
-//		now := &MyLinkedList{
-//			Val:  val,
-//			Next: this.Next,
-//		}
-//		this.Next.Prev = now
-//		this.Next = now
-//	}
-//	this.Val++
-//}
-//
-///** Append a node of value val to the last element of the linked list. */
-//func (this *MyLinkedList) AddAtTail(val int) {
-//	if this.Val == 0 {
-//		this.addFirst(val)
-//	} else {
-//		now := &MyLinkedList{
-//			Val:  val,
-//			Prev: this.Prev,
-//		}
-//		this.Prev.Next = now
-//		this.Prev = now
-//	}
-//	this.Val++
-//}
-//
-///** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
-//func (this *MyLinkedList) AddAtIndex(index int, val int) {
-//	if index <= 0 {
-//		this.AddAtHead(val)
-//		return
-//	} else if index == this.Val {
-//		this.AddAtTail(val)
-//		return
-//	} else if index > this.Val {
-//		return
-//	}
-//	node := this.findIndex(index)
-//	now := &MyLinkedList{
-//		Val:  val,
-//		Next: node.Next,
-//		Prev: node,
-//	}
-//	node.Prev.Next = now
-//	now.Prev = node.Prev
-//	now.Next = node
-//	node.Prev = now
-//	this.Val++
-//}
-//
-///** Delete the index-th node in the linked list, if the index is valid. */
-//func (this *MyLinkedList) DeleteAtIndex(index int) {
-//	if index < 0 || index >= this.Val || this.Val == 0 {
-//		return
-//	}
-//	if this.Val == 1 {
-//		this.Next = nil
-//		this.Prev = nil
-//	} else if index == 0 {
-//		this.Next = this.Next.Next
-//		this.Next.Prev = nil
-//	} else if this.Val == index-1 {
-//		this.Prev = this.Prev.Prev
-//		this.Prev.Next = nil
-//	} else {
-//		node := this.findIndex(index)
-//		node.Prev.Next = node.Next
-//		node.Next.Prev = node.Prev
-//	}
-//	this.Val--
-//}
--- question_701_800/question_701_710/707_design_linked_list_test.go
+++ question_701_800/question_701_710/707_design_linked_list_test.go
@@ -25,93 +25,50 @@ import (
 //	}
 //}
 
-func TestConstructor_1(t *testing.T) {
-	this := Constructor5()
-	this.AddAtHead(1)
-	fmt.Printf("%+v\n", this)
-	this.AddAtTail(3)
-	fmt.Printf("%+v\n", this)
-	this.AddAtIndex(1, 2)
-	fmt.Printf("%+v\n", this)
-
-	fmt.Printf("%d\n", this.Get(1))
-
-	this.DeleteAtIndex(1)
-	fmt.Printf("%+v\n", this)
-
-	fmt.Printf("%d\n", this.Get(1))
-}
-
-func TestConstructor_2(t *testing.T) {
-
-	// ["addAtHead","addAtHead","addAtHead", [7],[2],[1],
-	this := Constructor5()
-	this.AddAtHead(7)
-	fmt.Printf("%+v\n", this)
-	this.AddAtHead(2)
-	fmt.Printf("%+v\n", this)
-	this.AddAtHead(1)
-	fmt.Printf("%+v\n", this)
-
-	// addAtIndex [3,0],
-	this.AddAtIndex(3, 0)
-	fmt.Printf("%+v\n", this)
-
-	// deleteAtIndex [2],
-	this.DeleteAtIndex(2)
-	fmt.Printf("%+v\n", this)
-
-	// addAtHead [6],
-	this.AddAtHead(6)
-	fmt.Printf("%+v\n", this)
-
-	// addAtTail [4],
-	this.AddAtTail(4)
-	fmt.Printf("%+v\n", this)
-
-	// get [4],
-	fmt.Printf("%d\n", this.Get(4))
-
-	// addAtHead [4],
-	this.AddAtHead(4)
-	fmt.Printf("%+v\n", this)
-
-	// addAtIndex [5,0]
-	this.AddAtIndex(5, 0)
-	fmt.Printf("%+v\n", this)
-
-	// addAtHead [6]
-	this.AddAtHead(6)
-	fmt.Printf("%+v\n", this)
-}
-
-func TestConstructor_3(t *testing.T) {
-	this := Constructor5()
-	this.AddAtHead(1)
-	fmt.Printf("%+v\n", this)
-	this.DeleteAtIndex(1)
-	fmt.Printf("%+v\n", this)
-	this.DeleteAtIndex(0)
-	fmt.Printf("%+v\n", this)
-	this.AddAtTail(3)
-	fmt.Printf("%+v\n", this)
-	this.AddAtIndex(1, 2)
-	fmt.Printf("%+v\n", this)
-
-	fmt.Printf("%d\n", this.Get(1))
-
-	this.DeleteAtIndex(0)
-	fmt.Printf("%+v\n", this)
-
-	fmt.Printf("%d\n", this.Get(0))
-}
-
-func TestMyLinkedList_AddAtHead(t *testing.T) {
-	head := Constructor5()
-	head.AddAtHead(4)
-	head.AddAtHead(3)
-	head.AddAtHead(2)
-	head.AddAtHead(1)
-
-	fmt.Printf("%+v\n", head)
+func TestConstructor5(t *testing.T) {
+	tests := []struct {
+		funcs []string
+		args  [][]int
+	}{
+		{
+			funcs: []string{"MyLinkedList", "addAtHead", "deleteAtIndex", "addAtHead", "addAtHead", "addAtHead", "addAtHead", "addAtHead", "addAtTail", "get", "deleteAtIndex", "deleteAtIndex"},
+			args:  [][]int{{2}, {1}, {2}, {7}, {3}, {2}, {5}, {5}, {5}, {6}, {4}},
+		},
+		{
+			funcs: []string{"MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"},
+			args:  [][]int{{1}, {3}, {1, 2}, {1}, {0}, {0}},
+		},
+		{
+			funcs: []string{"MyLinkedList", "addAtHead", "deleteAtIndex", "addAtHead", "addAtHead", "addAtHead", "addAtHead", "addAtHead", "addAtTail", "get", "deleteAtIndex", "deleteAtIndex"},
+			args:  [][]int{{2}, {1}, {2}, {7}, {3}, {2}, {5}, {5}, {5}, {6}, {4}},
+		},
+		{
+			funcs: []string{"MyLinkedList", "addAtHead", "deleteAtIndex", "addAtHead", "addAtHead", "addAtHead", "addAtHead", "addAtHead", "addAtTail", "get", "deleteAtIndex", "deleteAtIndex"},
+			args:  [][]int{{2}, {1}, {2}, {7}, {3}, {2}, {5}, {5}, {5}, {6}, {4}},
+		},
+		{
+			funcs: []string{"MyLinkedList", "addAtHead", "deleteAtIndex", "addAtHead", "addAtHead", "addAtHead", "addAtHead", "addAtHead", "addAtTail", "get", "deleteAtIndex", "deleteAtIndex"},
+			args:  [][]int{{2}, {1}, {2}, {7}, {3}, {2}, {5}, {5}, {5}, {6}, {4}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run("test", func(t *testing.T) {
+			this := Constructor5()
+			for i, fun := range tt.funcs[1:] {
+				switch fun {
+				case "addAtHead":
+					this.AddAtHead(tt.args[i][0])
+				case "addAtTail":
+					this.AddAtTail(tt.args[i][0])
+				case "addAtIndex":
+					this.AddAtIndex(tt.args[i][0], tt.args[i][1])
+				case "deleteAtIndex":
+					this.DeleteAtIndex(tt.args[i][0])
+				case "get":
+					fmt.Printf("%d: Get:%d\n", i, this.Get(tt.args[i][0]))
+				}
+				fmt.Printf("%d: %s %+v %+v\n", i, fun, tt.args[i], this)
+			}
+		})
+	}
 }
