0afb9e12e4c91156b60da78c3215517c053bfda4
lupe <lupe.luo@klook.com>
2020-02-21 09:24:39 +0800

    849. 到最近的人的最大距离

--- question_801_900/question_841_850/849_maximize_distance_to_closest_person.go
+++ question_801_900/question_841_850/849_maximize_distance_to_closest_person.go
@@ -5,21 +5,20 @@ package question_841_850
 // Topics: 数组
 
 func maxDistToClosest(seats []int) int {
-	var tmp []int
+	var last = -1
+	var max = 0
 	for i, f := range seats {
 		if f == 1 {
-			tmp = append(tmp, i)
+			if last == -1 {
+				max = i
+			} else if (i-last)/2 > max {
+				max = (i - last) / 2
+			}
+			last = i
 		}
 	}
-	var max = tmp[0]
-	for i, n := range tmp {
-		if len(tmp)-1 == i {
-			if len(seats)-n-1 > max {
-				max = len(seats) - n - 1
-			}
-		} else if (tmp[i+1]-n)/2 > max {
-			max = (tmp[i+1] - n) / 2
-		}
+	if len(seats)-last-1 > max {
+		max = len(seats) - last - 1
 	}
 	return max
 }
