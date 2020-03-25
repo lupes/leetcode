1ae34bacdfbe1e224182fc6117c496da4aa0e606
lupe <lupe.luo@klook.com>
2020-03-25 12:48:04 +0800

    902. 最大为 N 的数字组合

--- question_901_1000/question_901_910/902_numbers_at_most_n_given_digit_set.go
+++ question_901_1000/question_901_910/902_numbers_at_most_n_given_digit_set.go
@@ -5,5 +5,50 @@ package question_901_910
 // Topics: 数学 动态规划
 
 func atMostNGivenDigitSet(D []string, N int) int {
-	return 0
+	l, t := 0, N
+	for t > 0 {
+		l++
+		t /= 10
+	}
+	var nD []int
+	for _, d := range D {
+		nD = append(nD, int(d[0])-'0')
+	}
+	return atMostNGivenDigitSetHelper(nD, N, l, 0)
+}
+
+func atMostNGivenDigitSetHelper(D []int, N int, i int, t int) int {
+	if i == -1 && t != 0 {
+		return 1
+	} else if i == -1 && t == 0 {
+		return 0
+	}
+	var res = 0
+	if t == 0 {
+		res += atMostNGivenDigitSetHelper(D, N, i-1, t)
+	}
+	for _, n := range D {
+		x := to(n, i)
+		if t+x <= N {
+			res += atMostNGivenDigitSetHelper(D, N, i-1, t+x)
+		} else {
+			break
+		}
+	}
+	return res
+}
+
+func to(i, b int) int {
+	for j := 0; j < b; j++ {
+		i *= 10
+	}
+	return i
+}
+
+func pow(x, y int) int {
+	var res = 1
+	for j := 0; j < y; j++ {
+		res *= x
+	}
+	return res
 }
---
+++ question_901_1000/question_901_910/902_numbers_at_most_n_given_digit_set_test.go
@@ -0,0 +1,23 @@
+package question_901_910
+
+import (
+	"testing"
+)
+
+func Test_atMostNGivenDigitSet(t *testing.T) {
+	tests := []struct {
+		D    []string
+		N    int
+		want int
+	}{
+		{[]string{"1", "3", "5", "7"}, 100, 20},
+		{[]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}, 899894860, 392738517},
+	}
+	for _, tt := range tests {
+		t.Run("test", func(t *testing.T) {
+			if got := atMostNGivenDigitSet(tt.D, tt.N); got != tt.want {
+				t.Errorf("atMostNGivenDigitSet() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
