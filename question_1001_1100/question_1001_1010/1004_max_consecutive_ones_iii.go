a2af22e2c732cede2d46ea7319c11d08459e717b
lupe <lupe.luo@klook.com>
2020-04-25 19:26:22 +0800

    1004. 最大连续1的个数 III

--- question_1001_1100/question_1001_1010/1004_max_consecutive_ones_iii.go
+++ question_1001_1100/question_1001_1010/1004_max_consecutive_ones_iii.go
@@ -5,5 +5,23 @@ package question_1001_1010
 // Topics: 双指针 None
 
 func longestOnes(A []int, K int) int {
-	return 0
+	left, right, tmp, res := 0, 0, 0, 0
+	for len(A) > left && len(A) > right {
+		if tmp <= K {
+			if A[right] == 0 {
+				tmp++
+			}
+			if tmp <= K && right-left+1 > res {
+				res = right - left + 1
+			}
+			right++
+		} else {
+			if A[left] == 0 {
+				tmp--
+			}
+			left++
+		}
+		//fmt.Printf("left:%d right:%d tmp:%d res:%d\n", left, right, tmp, res)
+	}
+	return res
 }
