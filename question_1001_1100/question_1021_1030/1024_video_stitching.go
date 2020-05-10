6bd2d4b55af2e51f162c5429dcfaec721daa586a
lupe <lupe.luo@klook.com>
2020-05-10 20:23:16 +0800

    1024. 视频拼接

--- question_1001_1100/question_1021_1030/1024_video_stitching.go
+++ question_1001_1100/question_1021_1030/1024_video_stitching.go
@@ -1,9 +1,34 @@
 package question_1021_1030
 
+import (
+	"sort"
+)
+
 // 1024. 视频拼接
 // https://leetcode-cn.com/problems/video-stitching
 // Topics: 动态规划
 
 func videoStitching(clips [][]int, T int) int {
-	return 0
+	var res = make([]int, T+1)
+	sort.Slice(clips, func(i, j int) bool {
+		return clips[i][0] < clips[j][0]
+	})
+	for _, clip := range clips {
+		if clip[0] >= T {
+			break
+		}
+		tmp := res[clip[0]] + 1
+		if clip[0] != 0 && tmp == 1 {
+			return -1
+		}
+		for i := clip[0] + 1; i <= clip[1] && i <= T; i++ {
+			if res[i] == 0 || res[i] > tmp {
+				res[i] = tmp
+			}
+		}
+	}
+	if res[T] == 0 {
+		return -1
+	}
+	return res[T]
 }
