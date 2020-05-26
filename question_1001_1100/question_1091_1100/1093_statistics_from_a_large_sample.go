3b795b03aa44bae193fe2bfdd8f9050bccd3ca91
lupe <lupe.luo@klook.com>
2020-05-26 14:05:09 +0800

    1093. 大样本统计

--- question_1001_1100/question_1091_1100/1093_statistics_from_a_large_sample.go
+++ question_1001_1100/question_1091_1100/1093_statistics_from_a_large_sample.go
@@ -5,5 +5,48 @@ package question_1091_1100
 // Topics: 数学 双指针
 
 func sampleStats(count []int) []float64 {
-	return nil
+	var maxZ = 0
+	var index = 0
+	var sum = 0
+	var num = 0
+	var min, max = -1, -1
+	for i, n := range count {
+		if n == 0 {
+			continue
+		}
+		if min == -1 {
+			min = i
+		}
+		max = i
+		sum += i * n
+		if n > maxZ {
+			maxZ = n
+			index = i
+		}
+		num += n
+	}
+	an, bn := num/2+1, num/2+1
+	if num%2 == 0 {
+		an, bn = num/2, num/2+1
+	}
+
+	a, b := 0, 0
+	for i, n := range count {
+		if n == 0 {
+			continue
+		}
+		if an-n > 0 {
+			an -= n
+		} else if an > 0 {
+			an -= n
+			a = i
+		}
+		if bn-n > 0 {
+			bn -= n
+		} else {
+			b = i
+			break
+		}
+	}
+	return []float64{float64(min), float64(max), float64(sum) / float64(num), (float64(a) + float64(b)) / 2, float64(index)}
 }
