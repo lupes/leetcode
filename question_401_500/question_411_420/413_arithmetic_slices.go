f551f4167dad0dde2b77427bdc9a2b91c0bc3fa0
lupe <lupe.luo@klook.com>
2020-07-05 14:55:39 +0800

    413. 等差数列划分

--- question_401_500/question_411_420/413_arithmetic_slices.go
+++ question_401_500/question_411_420/413_arithmetic_slices.go
@@ -5,5 +5,12 @@ package question_411_420
 // Topics: 数学 动态规划
 
 func numberOfArithmeticSlices(A []int) int {
-	return 0
+	var tmp, res = make([]int, len(A)), 0
+	for i := 2; i < len(A); i++ {
+		if A[i]-A[i-1] == A[i-1]-A[i-2] {
+			tmp[i] = tmp[i-1] + 1
+		}
+		res += tmp[i]
+	}
+	return res
 }
