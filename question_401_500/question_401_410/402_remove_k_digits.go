46278bb358f9b73968e35830920a8932e9fe0fce
lupe <lupe.luo@klook.com>
2020-07-06 15:04:55 +0800

    402. 移掉K位数字

--- question_401_500/question_401_410/402_remove_k_digits.go
+++ question_401_500/question_401_410/402_remove_k_digits.go
@@ -5,5 +5,28 @@ package question_401_410
 // Topics: 栈 贪心算法
 
 func removeKdigits(num string, k int) string {
-	return ""
+	if len(num) == k {
+		return "0"
+	}
+	var stack, res = make([]uint8, 0, len(num)), ""
+	for i := 0; i < len(num); {
+		if len(stack) > 0 && k > 0 && stack[len(stack)-1] > num[i] {
+			stack = stack[:len(stack)-1]
+			k--
+		} else {
+			stack = append(stack, num[i])
+			i++
+		}
+	}
+	for _, n := range stack {
+		if (len(res) == 0 && n != '0') || (len(res) != 0) {
+			res += string(n)
+		}
+	}
+	if k > 0 && k < len(res) {
+		res = res[:len(res)-k]
+	} else if len(res) == 0 {
+		res = "0"
+	}
+	return res
 }
---
+++ question_401_500/question_401_410/402_remove_k_digits_test.go
@@ -0,0 +1,29 @@
+package question_401_410
+
+import (
+	"testing"
+)
+
+func Test_removeKdigits(t *testing.T) {
+	tests := []struct {
+		num  string
+		k    int
+		want string
+	}{
+		{"1432219", 3, "1219"},
+		{"1234567890", 9, "0"},
+		{"123456789", 8, "1"},
+		{"123456789", 9, "0"},
+		{"1432219", 4, "119"},
+		{"10200", 1, "200"},
+		{"10200", 2, "0"},
+		{"10", 2, "0"},
+	}
+	for _, tt := range tests {
+		t.Run("test", func(t *testing.T) {
+			if got := removeKdigits(tt.num, tt.k); got != tt.want {
+				t.Errorf("removeKdigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
