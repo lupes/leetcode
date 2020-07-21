31112d8375e881aae5f19fb08c963acc9d4bd5f2
lupe <lupe.luo@klook.com>
2020-07-21 22:07:11 +0800

    529. 扫雷游戏

--- question_501_600/question_521_530/529_minesweeper.go
+++ question_501_600/question_521_530/529_minesweeper.go
@@ -4,6 +4,42 @@ package question_521_530
 // https://leetcode-cn.com/problems/minesweeper
 // Topics: 深度优先搜索 广度优先搜索
 
+var boardFlag = [][2]int{
+	{-1, -1}, {-1, 0}, {-1, 1},
+	{0, -1}, {0, 1},
+	{1, -1}, {1, 0}, {1, 1},
+}
+
 func updateBoard(board [][]byte, click []int) [][]byte {
-	return nil
+	if board[click[0]][click[1]] == 'M' {
+		board[click[0]][click[1]] = 'X'
+		return board
+	}
+	updateBoardHelper(board, click, len(board), len(board[0]))
+	return board
+}
+
+func updateBoardHelper(board [][]byte, click []int, cl, rl int) {
+	var t, a, b = byte(0), click[0], click[1]
+	for _, f := range boardFlag {
+		x, y := click[0]+f[0], click[1]+f[1]
+		if x >= 0 && y >= 0 && x < cl && y < rl {
+			if board[x][y] == 'M' {
+				t++
+			}
+		}
+	}
+	if t > 0 {
+		board[a][b] = '0' + t
+	} else if t == 0 {
+		board[a][b] = 'B'
+		for _, f := range boardFlag {
+			x, y := click[0]+f[0], click[1]+f[1]
+			if x >= 0 && y >= 0 && x < cl && y < rl {
+				if board[x][y] == 'E' {
+					updateBoardHelper(board, []int{x, y}, cl, rl)
+				}
+			}
+		}
+	}
 }
---
+++ question_501_600/question_521_530/529_minesweeper_test.go
@@ -0,0 +1,67 @@
+package question_521_530
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_updateBoard(t *testing.T) {
+	tests := []struct {
+		board [][]byte
+		click []int
+		want  [][]byte
+	}{
+		{
+			[][]byte{
+				{'E', 'E', 'E', 'E', 'E'},
+				{'E', 'E', 'M', 'E', 'E'},
+				{'E', 'E', 'E', 'E', 'E'},
+				{'E', 'E', 'E', 'E', 'E'},
+			},
+			[]int{3, 0},
+			[][]byte{
+				{'B', '1', 'E', '1', 'B'},
+				{'B', '1', 'M', '1', 'B'},
+				{'B', '1', '1', '1', 'B'},
+				{'B', 'B', 'B', 'B', 'B'},
+			},
+		},
+		{
+			[][]byte{
+				{'B', '1', 'E', '1', 'B'},
+				{'B', '1', 'M', '1', 'B'},
+				{'B', '1', '1', '1', 'B'},
+				{'B', 'B', 'B', 'B', 'B'},
+			},
+			[]int{0, 2},
+			[][]byte{
+				{'B', '1', '1', '1', 'B'},
+				{'B', '1', 'M', '1', 'B'},
+				{'B', '1', '1', '1', 'B'},
+				{'B', 'B', 'B', 'B', 'B'},
+			},
+		},
+		{
+			[][]byte{
+				{'B', '1', 'E', '1', 'B'},
+				{'B', '1', 'M', '1', 'B'},
+				{'B', '1', '1', '1', 'B'},
+				{'B', 'B', 'B', 'B', 'B'},
+			},
+			[]int{1, 2},
+			[][]byte{
+				{'B', '1', 'E', '1', 'B'},
+				{'B', '1', 'X', '1', 'B'},
+				{'B', '1', '1', '1', 'B'},
+				{'B', 'B', 'B', 'B', 'B'},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run("test", func(t *testing.T) {
+			if got := updateBoard(tt.board, tt.click); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("updateBoard() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
