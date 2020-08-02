305493a77c3109b73730a1d6a136ffac56ac6c7f
lupe <lupe.luo@klook.com>
2020-08-02 22:27:11 +0800

    738. 单调递增的数字

--- question_701_800/question_731_740/738_monotone_increasing_digits.go
+++ question_701_800/question_731_740/738_monotone_increasing_digits.go
@@ -7,28 +7,20 @@ package question_731_740
 func monotoneIncreasingDigits(N int) int {
 	var flag = make([]int, 0)
 	for N > 0 {
-		flag = append([]int{N % 10}, flag...)
+		flag = append(flag, N%10)
 		N /= 10
 	}
 	for i := 1; i < len(flag); i++ {
-		if flag[i] < flag[i-1] {
-			for j := i; j > 0; j-- {
-				if flag[j] < flag[j-1] {
-					flag[j] = 9
-					flag[j-1] -= 1
-				} else {
-					break
-				}
-			}
-			for j := i; j < len(flag); j++ {
+		if flag[i] > flag[i-1] {
+			flag[i] -= 1
+			for j := i - 1; j >= 0 && flag[j] != 9; j-- {
 				flag[j] = 9
 			}
-			break
 		}
 	}
 	var res int
-	for _, n := range flag {
-		res = res*10 + n
+	for i := len(flag) - 1; i >= 0; i-- {
+		res = res*10 + flag[i]
 	}
 	return res
 }
