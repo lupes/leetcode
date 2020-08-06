8e6b2c7b542ebda9032b11ed2f1491ede6ee12f8
lupe <lupe.luo@klook.com>
2020-08-06 09:44:51 +0800

    754. 到达终点数字

--- question_701_800/question_751_760/754_reach_a_number.go
+++ question_701_800/question_751_760/754_reach_a_number.go
@@ -1,5 +1,9 @@
 package question_751_760
 
+import (
+	"math"
+)
+
 // 754. 到达终点数字
 // https://leetcode-cn.com/problems/reach-a-number
 // Topics: 数学
@@ -8,7 +12,8 @@ func reachNumber(target int) int {
 	if target < 0 {
 		target *= -1
 	}
-	for i, tmp := 1, 0; ; i++ {
+	n := int(math.Sqrt(float64(target*2))) - 1
+	for i, tmp := n+1, (n+1)*n/2; ; i++ {
 		tmp += i
 		if tmp == target || (tmp > target && (tmp-target)%2 == 0) {
 			return i
