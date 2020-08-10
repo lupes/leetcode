49344b790e0600e168db2bf9b7a6ad522bd8eb7a
lupe <lupe.luo@klook.com>
2020-08-10 19:40:28 +0800

    976. 三角形的最大周长

--- question_901_1000/question_971_980/976_largest_perimeter_triangle.go
+++ question_901_1000/question_971_980/976_largest_perimeter_triangle.go
@@ -1,9 +1,20 @@
 package question_971_980
 
+import (
+	"sort"
+)
+
 // 976. 三角形的最大周长
 // https://leetcode-cn.com/problems/largest-perimeter-triangle
 // Topics: 排序 数学
 
 func largestPerimeter(A []int) int {
-	return 0
+	sort.Ints(A)
+	var max = 0
+	for i := len(A) - 1; i >= 2; i-- {
+		if A[i-2]+A[i-1] > A[i] {
+			return A[i] + A[i-1] + A[i-2]
+		}
+	}
+	return max
 }
