9c90cc19996b891e1c402841d85e2c2ea76f38bc
lupe <lupe.luo@klook.com>
2020-10-19 19:08:31 +0800

    117. 填充每个节点的下一个右侧节点指针 II

--- question_101_200/question_111_120/117_populating_next_right_pointers_in_each_node_ii.go
+++ question_101_200/question_111_120/117_populating_next_right_pointers_in_each_node_ii.go
@@ -1,16 +1,23 @@
-# 117. 填充每个节点的下一个右侧节点指针 II
+package question_111_120
+
+// 117. 填充每个节点的下一个右侧节点指针 II
 // https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii
 // Topics: 树 深度优先搜索
 
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val, left, right, next):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-class Solution:
-    def connect(self, root: 'Node') -> 'Node':
-        
+func connect2(root *Node) *Node {
+	next := &Node{Val: 0, Left: nil, Right: nil, Next: nil}
+	now, head := next, root
+	for head != nil {
+		for head != nil {
+			if head.Left != nil {
+				now.Next, now = head.Left, head.Left
+			}
+			if head.Right != nil {
+				now.Next, now = head.Right, head.Right
+			}
+			head = head.Next
+		}
+		head, next.Next, now = next.Next, nil, next
+	}
+	return root
+}
