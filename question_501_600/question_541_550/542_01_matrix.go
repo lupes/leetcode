f317cfbbabbc21f7dc46ded3acce463dc018a293
lupe <lupe.luo@klook.com>
2020-11-19 19:13:25 +0800

    542. 01 矩阵

--- question_501_600/question_541_550/542_01_matrix.go
+++ question_501_600/question_541_550/542_01_matrix.go
@@ -4,54 +4,33 @@ package question_541_550
 // https://leetcode-cn.com/problems/01-matrix/
 // Topics: 深度优先搜索 广度优先搜索
 
+var matrixFlag = [][]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+
 func updateMatrix(matrix [][]int) [][]int {
 	r := len(matrix)
 	if r < 0 {
 		return matrix
 	}
 	c := len(matrix[0])
+	queue, point := make([][2]int, 0), [2]int{0, 0}
 	for i := 0; i < r; i++ {
 		for j := 0; j < c; j++ {
-			if matrix[i][j] == 1 {
-				matrix[i][j] = -1
+			matrix[i][j] *= -1
+			if matrix[i][j] == 0 {
+				queue = append(queue, [2]int{i, j})
 			}
 		}
 	}
-	for i := 0; i < r; i++ {
-		for j := 0; j < c; j++ {
-			if matrix[i][j] == -1 {
-				l, _ := updateMatrixDfs(matrix, r, c, i, j)
-				matrix[i][j] = l
+	for len(queue) > 0 {
+		point, queue = queue[0], queue[1:]
+		for _, p := range matrixFlag {
+			i, j := point[0]+p[0], point[1]+p[1]
+			if i >= 0 && i < r && j >= 0 && j < c && matrix[i][j] == -1 {
+				matrix[i][j] = matrix[point[0]][point[1]] + 1
+				queue = append(queue, [2]int{i, j})
 			}
 		}
 	}
-	return matrix
-}
 
-func updateMatrixDfs(matrix [][]int, r, c, i, j int) (int, bool) {
-	if (i == r || i == -1) || (j == c || j == -1) {
-		return 0, false
-	}
-	if matrix[i][j] == -2 {
-		return 0, false
-	}
-	if matrix[i][j] >= 0 {
-		return matrix[i][j], true
-	}
-	matrix[i][j] = -2
-	l := r + c
-	if t, ok := updateMatrixDfs(matrix, r, c, i, j-1); ok && t < l {
-		l = t
-	}
-	if t, ok := updateMatrixDfs(matrix, r, c, i-1, j); ok && t < l {
-		l = t
-	}
-	if t, ok := updateMatrixDfs(matrix, r, c, i, j+1); ok && t < l {
-		l = t
-	}
-	if t, ok := updateMatrixDfs(matrix, r, c, i+1, j); ok && t < l {
-		l = t
-	}
-	matrix[i][j] = -1
-	return l + 1, true
+	return matrix
 }
