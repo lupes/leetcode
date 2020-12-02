6fbaa1c3d0e8f200fe9bb6ba03b56019c8460012
lupe <lupe.luo@klook.com>
2020-12-02 19:26:09 +0800

    17. 电话号码的字母组合

--- question_001_100/question_11_20/17_letter_combinations_of_a_phone_number.go
+++ question_001_100/question_11_20/17_letter_combinations_of_a_phone_number.go
@@ -1,25 +1,28 @@
 package question_11_20
 
-var base = []string{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
-
 // 17. 电话号码的字母组合
 // https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
 // Topics: 字符串 回溯算法
 
+var base = []string{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
+
 func letterCombinations(digits string) []string {
 	if digits == "" {
-		return []string{}
+		return nil
 	}
-	res := []string{""}
-	for _, c := range digits {
-		var tmp []string
-		strs := base[c-'2']
-		for _, str := range res {
-			for _, char := range strs {
-				tmp = append(tmp, str+string(char))
-			}
-		}
-		res = tmp
-	}
+	var res []string
+	letterCombinationsHelper(digits, &res, "")
 	return res
+}
+
+func letterCombinationsHelper(digits string, res *[]string, tmp string) {
+	if digits == "" {
+		*res = append(*res, tmp)
+		return
+	}
+	chars := base[digits[0]-'2']
+	for _, c := range chars {
+		letterCombinationsHelper(digits[1:], res, tmp+string(c))
+	}
+	return
 }
--- question_001_100/question_11_20/17_letter_combinations_of_a_phone_number_test.go
+++ question_001_100/question_11_20/17_letter_combinations_of_a_phone_number_test.go
@@ -6,20 +6,17 @@ import (
 )
 
 func Test_letterCombinations(t *testing.T) {
-	type args struct {
-		digits string
-	}
 	tests := []struct {
-		name string
-		args args
-		want []string
+		digits string
+		want   []string
 	}{
-		{"test#1", args{""}, []string{}},
-		{"test#2", args{"23"}, []string{"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"}},
+		{"", nil},
+		{"2", []string{"a", "b", "c"}},
+		{"23", []string{"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"}},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := letterCombinations(tt.args.digits); !reflect.DeepEqual(got, tt.want) {
+		t.Run("test", func(t *testing.T) {
+			if got := letterCombinations(tt.digits); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("letterCombinations() = %v, want %v", got, tt.want)
 			}
 		})
