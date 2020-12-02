d019b382306c0eb0adeb07b975bb3d56ad76072a
lupe <lupe.luo@klook.com>
2020-12-02 20:02:16 +0800

    21. 合并两个有序链表

--- question_001_100/question_21_30/21_merge_two_sorted_lists.go
+++ question_001_100/question_21_30/21_merge_two_sorted_lists.go
@@ -1,34 +1,27 @@
 package question_0011_0020
 
+import (
+	. "github.com/lupes/leetcode/common"
+)
+
 // 21. 合并两个有序链表
 // https://leetcode-cn.com/problems/merge-two-sorted-lists
 // Topics: 链表
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	if l1 == nil {
-		return l2
-	}
-	if l2 == nil {
-		return l1
-	}
-	if l1.Val > l2.Val {
-		l1, l2 = l2, l1
-	}
-	head := l1
-	last := l1
-	for l1 != nil && l2 != nil {
-		if l2.Val < l1.Val {
-			last.Next = l2
-			last = last.Next
-			l2 = l2.Next
-			last.Next = l1
+	head := &ListNode{}
+	next := head
+	for l1 != nil || l2 != nil {
+		if l1 == nil {
+			next.Next, l2 = l2, nil
+		} else if l2 == nil {
+			next.Next, l1 = l1, nil
+		} else if l1.Val > l2.Val {
+			next.Next, l2 = l2, l2.Next
 		} else {
-			last = l1
-			l1 = l1.Next
+			next.Next, l1 = l1, l1.Next
 		}
+		next = next.Next
 	}
-	if l2 != nil {
-		last.Next = l2
-	}
-	return head
+	return head.Next
 }
--- question_001_100/question_21_30/21_merge_two_sorted_lists_test.go
+++ question_001_100/question_21_30/21_merge_two_sorted_lists_test.go
@@ -3,59 +3,28 @@ package question_0011_0020
 import (
 	"reflect"
 	"testing"
+
+	. "github.com/lupes/leetcode/common"
 )
 
 func Test_mergeTwoLists(t *testing.T) {
-	var (
-		t0l1 *ListNode
-		t0l2 *ListNode
-		t0w  *ListNode
-
-		t1l1 = &ListNode{Val: 1}
-		t1l2 *ListNode
-		t1w  = &ListNode{Val: 1}
-
-		t2l1 *ListNode
-		t2l2 = &ListNode{Val: 1}
-		t2w  = &ListNode{Val: 1}
-
-		t3l1 = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}
-		t3l2 = &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}
-		t3w  = &ListNode{Val: 1, Next: &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 4}}}}}}
-
-		t4l1 = &ListNode{Val: 1, Next: &ListNode{Val: 3}}
-		t4l2 = &ListNode{Val: 2, Next: &ListNode{Val: 4}}
-		t4w  = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
-	)
 	tests := []struct {
-		name string
 		l1   *ListNode
 		l2   *ListNode
 		want *ListNode
 	}{
-		{"test#0", t0l1, t0l2, t0w},
-		{"test#1", t1l1, t1l2, t1w},
-		{"test#2", t2l1, t2l2, t2w},
-		{"test#3", t3l1, t3l2, t3w},
-		{"test#4", t4l1, t4l2, t4w},
+		{nil, nil, nil},
+		{NewList(1), nil, NewList(1)},
+		{nil, NewList(1), NewList(1)},
+		{NewList(1, 2, 4), NewList(1, 3, 4), NewList(1, 1, 2, 3, 4, 4)},
+		{NewList(1, 3), NewList(2, 4), NewList(1, 2, 3, 4)},
+		{NewList(1, 3, 3), NewList(2, 4), NewList(1, 2, 3, 3, 4)},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run("test", func(t *testing.T) {
 			got := mergeTwoLists(tt.l1, tt.l2)
-			want := tt.want
-			var arr1 []int
-			var arr2 []int
-			for got != nil {
-				arr1 = append(arr1, got.Val)
-				got = got.Next
-			}
-
-			for want != nil {
-				arr2 = append(arr2, want.Val)
-				want = want.Next
-			}
-			if !reflect.DeepEqual(arr1, arr2) {
-				t.Errorf("mergeTwoLists() = %v, want %v", arr1, arr2)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeTwoLists() = %v, want %v", got, tt.want)
 			}
 		})
 	}
