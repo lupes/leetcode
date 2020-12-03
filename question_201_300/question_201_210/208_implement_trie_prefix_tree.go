eb169f28d4ab5b0ee921e8de1f5a1385016c97bf
lupe <lupe.luo@klook.com>
2020-12-03 20:20:43 +0800

    208. 实现 Trie (前缀树)

--- question_201_300/question_201_210/208_implement_trie_prefix_tree.go
+++ question_201_300/question_201_210/208_implement_trie_prefix_tree.go
@@ -1,47 +1,59 @@
 package question_201_210
 
-import "strings"
-
 // 208. 实现 Trie (前缀树)
 // https://leetcode-cn.com/problems/implement-trie-prefix-tree/
 // Topics: 设计 字典树
 
 type Trie struct {
-	DataMap map[string]struct{}
+	End bool
+	Son map[uint8]*Trie
 }
 
 /** Initialize your data structure here. */
 func Constructor() Trie {
 	return Trie{
-		DataMap: make(map[string]struct{}, 0),
+		End: false,
+		Son: make(map[uint8]*Trie, 26),
 	}
 }
 
 /** Inserts a word into the trie. */
 func (this *Trie) Insert(word string) {
-	this.DataMap[word] = struct{}{}
+	if len(word) == 0 {
+		this.End = true
+		return
+	}
+	son, ok := this.Son[word[0]]
+	if !ok {
+		son = &Trie{
+			Son: make(map[uint8]*Trie, 26),
+		}
+	}
+	son.Insert(word[1:])
+	this.Son[word[0]] = son
 }
 
 /** Returns if the word is in the trie. */
 func (this *Trie) Search(word string) bool {
-	_, ok := this.DataMap[word]
-	return ok
+	if len(word) == 0 {
+		return this.End
+	}
+	son, ok := this.Son[word[0]]
+	if !ok {
+		return false
+	}
+	return son.Search(word[1:])
 }
 
 /** Returns if there is any word in the trie that starts with the given prefix. */
 func (this *Trie) StartsWith(prefix string) bool {
-	for key, _ := range this.DataMap {
-		if strings.HasPrefix(key, prefix) {
-			return true
-		}
+	if len(prefix) == 0 {
+		return true
 	}
-	return false
+	c, last := prefix[0], prefix[1:]
+	son, ok := this.Son[c]
+	if !ok {
+		return false
+	}
+	return son.StartsWith(last)
 }
-
-/**
- * Your Trie object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Insert(word);
- * param_2 := obj.Search(word);
- * param_3 := obj.StartsWith(prefix);
- */
--- question_201_300/question_201_210/208_implement_trie_prefix_tree_test.go
+++ question_201_300/question_201_210/208_implement_trie_prefix_tree_test.go
@@ -1,25 +1,46 @@
 package question_201_210
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestTrie(t *testing.T) {
-	trie := Constructor()
-	trie.Insert("apple")
-	if ok := trie.Search("apple"); !ok {
-		t.Errorf("Search(apple) = %v, want %v", ok, true)
-	}
+	//trie := Constructor()
+	//trie.Insert("apple")
+	//
+	//if ok := trie.Search("apple"); !ok {
+	//	t.Errorf("Search(apple) = %v, want %v", ok, true)
+	//}
+	//
+	//if ok := trie.Search("app"); ok {
+	//	t.Errorf("Search(app) = %v, want %v", ok, false)
+	//}
+	//
+	//if ok := trie.StartsWith("app"); !ok {
+	//	t.Errorf("StartsWith(app) = %v, want %v", ok, true)
+	//}
+	////
+	//trie.Insert("app")
+	//if ok := trie.Search("app"); !ok {
+	//	t.Errorf("Search(apple) = %v, want %v", ok, true)
+	//}
+	//
+	//if ok, got := trie.Search("app"), true; ok != got {
+	//	t.Errorf("Search(apple) = %v, want %v", ok, got)
+	//}
+	//
+	//if ok, got := trie.Search("ab"), false; ok != got {
+	//	t.Errorf("Search(apple) = %v, want %v", ok, got)
+	//}
+	//
+	//if ok, got := trie.Search("appl"), false; ok != got {
+	//	t.Errorf("Search(apple) = %v, want %v", ok, got)
+	//}
 
-	if ok := trie.Search("app"); ok {
-		t.Errorf("Search(apple) = %v, want %v", ok, false)
+	this := Constructor()
+	this.Insert("app")
+	this.Insert("apple")
+	if ok, got := this.Search("app"), true; ok != got {
+		t.Errorf("Search(apple) = %v, want %v", ok, got)
 	}
-
-	if ok := trie.StartsWith("app"); !ok {
-		t.Errorf("Search(apple) = %v, want %v", ok, true)
-	}
-
-	trie.Insert("app")
-	if ok := trie.Search("app"); !ok {
-		t.Errorf("Search(apple) = %v, want %v", ok, true)
-	}
-
 }
