5c680375dd45ceae7f5bbea1e99c4d9e7b848cae
lupe <lupe.luo@klook.com>
2020-12-15 19:49:06 +0800

    92. 反转链表 II

--- question_001_100/question_91_100/92_reverse_linked_list_ii.go
+++ question_001_100/question_91_100/92_reverse_linked_list_ii.go
@@ -1,6 +1,8 @@
 package question_91_100
 
-import . "github.com/lupes/leetcode/common"
+import (
+	. "github.com/lupes/leetcode/common"
+)
 
 // 92. 反转链表 II
 // https://leetcode-cn.com/problems/reverse-linked-list-ii
@@ -10,29 +12,16 @@ func reverseBetween(head *ListNode, m int, n int) *ListNode {
 	if head == nil {
 		return nil
 	}
-	start := &ListNode{Next: head}
-	next := head
-	var i = 1
-	var hEnd = start
-	var reverseStart = &ListNode{}
-	var reverseEnd *ListNode
-	for next != nil {
-		t := next.Next
-		if i >= m && i <= n {
-			if i == m {
-				reverseEnd = next
-			}
-			next.Next = reverseStart.Next
-			reverseStart.Next = next
-		} else if i < m {
-			hEnd = next
-		} else if i > n {
-			break
-		}
-		next = t
-		i++
+	head = &ListNode{Next: head}
+	start := head
+	for i := 1; i < m; i++ {
+		start = start.Next
 	}
-	reverseEnd.Next = next
-	hEnd.Next = reverseStart.Next
-	return start.Next
+	end, next := start.Next, start.Next
+	for i := 0; i < n-m; i++ {
+		next = end.Next
+		start.Next, next.Next, end.Next = next, start.Next, end.Next.Next
+	}
+
+	return head.Next
 }
--- question_001_100/question_91_100/92_reverse_linked_list_ii_test.go
+++ question_001_100/question_91_100/92_reverse_linked_list_ii_test.go
@@ -1,7 +1,6 @@
 package question_91_100
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 
@@ -9,57 +8,25 @@ import (
 )
 
 func Test_reverseBetween(t *testing.T) {
-	head1 := &ListNode{}
-	head2 := &ListNode{Val: 1, Next: &ListNode{Val: 2}}
-	want2 := &ListNode{Val: 2, Next: &ListNode{Val: 1}}
-
-	head3 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
-	want3 := &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
-
-	head4 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
-	want4 := &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: &ListNode{Val: 4}}}}
-
-	head5 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
-	want5 := &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 1}}}}
-
-	head6 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
-	want6 := &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}}
-
-	head7 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
-	want7 := &ListNode{Val: 1, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{Val: 2}}}}
-
-	head8 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
-	want8 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
 	tests := []struct {
-		name string
 		head *ListNode
 		m    int
 		n    int
 		want *ListNode
 	}{
-		{"test", nil, 0, 0, nil},
-		{"test", head1, 1, 1, head1},
-		{"test", head2, 1, 2, want2},
-		{"test", head3, 1, 2, want3},
-		{"test", head4, 1, 3, want4},
-		{"test", head5, 1, 4, want5},
-		{"test", head6, 2, 3, want6},
-		{"test", head7, 2, 4, want7},
-		{"test", head8, 4, 4, want8},
+		{nil, 0, 0, nil},
+		{NewList(), 1, 1, NewList()},
+		{NewList(1, 2), 1, 2, NewList(2, 1)},
+		{NewList(1, 2, 3, 4), 1, 2, NewList(2, 1, 3, 4)},
+		{NewList(1, 2, 3, 4), 1, 3, NewList(3, 2, 1, 4)},
+		{NewList(1, 2, 3, 4), 1, 4, NewList(4, 3, 2, 1)},
+		{NewList(1, 2, 3, 4), 2, 3, NewList(1, 3, 2, 4)},
+		{NewList(1, 2, 3, 4), 2, 4, NewList(1, 4, 3, 2)},
+		{NewList(1, 2, 3, 4), 4, 4, NewList(1, 2, 3, 4)},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run("test", func(t *testing.T) {
 			got := reverseBetween(tt.head, tt.m, tt.n)
-			fmt.Print("got:  ")
-			for next := got; next != nil; next = next.Next {
-				fmt.Printf("%d ", next.Val)
-			}
-			fmt.Println()
-			fmt.Print("want: ")
-			for next := tt.want; next != nil; next = next.Next {
-				fmt.Printf("%d ", next.Val)
-			}
-			fmt.Println()
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("got:%v want:%v\n", got, tt.want)
 			}
