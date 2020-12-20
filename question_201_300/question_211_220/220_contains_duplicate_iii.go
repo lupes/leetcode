96194bbbfb0e26b2491f2ad9cc81f2711646c1a1
lupe <lupe.luo@klook.com>
2020-12-20 19:46:57 +0800

    220. 存在重复元素 III

--- question_201_300/question_211_220/220_contains_duplicate_iii.go
+++ question_201_300/question_211_220/220_contains_duplicate_iii.go
@@ -1,15 +1,34 @@
 package question_211_220
 
-import "math"
+import (
+	"sort"
+)
 
 // 220. 存在重复元素 III
 // https://leetcode-cn.com/problems/contains-duplicate-iii/
 // Topics: 排序 None
 
 func containsNearbyAlmostDuplicate(nums []int, k int, t int) bool {
+	type N struct {
+		n int
+		i int
+	}
+	var tmp []N
 	for i, n := range nums {
-		for j := i + 1; j <= i+k && j < len(nums); j++ {
-			if math.Abs(float64(n-nums[j])) <= float64(t) {
+		tmp = append(tmp, N{
+			n: n,
+			i: i,
+		})
+	}
+
+	sort.Slice(tmp, func(i, j int) bool {
+		return tmp[i].n < tmp[j].n
+	})
+
+	for i, n := range tmp {
+		for j := i + 1; j < len(tmp) && n.n+t >= tmp[j].n; j++ {
+			a := n.i - tmp[j].i
+			if (a < 0 && -a <= k) || (a > 0 && k >= a) {
 				return true
 			}
 		}
