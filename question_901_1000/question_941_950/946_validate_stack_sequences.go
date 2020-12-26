143e09959684c518ae2694b3dfed8c0ee4907415
lupe <lupe.luo@klook.com>
2020-12-26 20:40:42 +0800

    897. 递增顺序查找树

--- question_901_1000/question_941_950/946_validate_stack_sequences.go
+++ question_901_1000/question_941_950/946_validate_stack_sequences.go
@@ -9,13 +9,12 @@ func validateStackSequences(pushed []int, popped []int) bool {
 	for {
 		if len(stack) == 0 && len(pushed) == 0 && len(popped) == 0 {
 			return true
-		} else if (len(pushed) > 0 && len(stack) == 0) ||
-			(len(pushed) > 0 && len(stack) > 0 && len(popped) > 0 && stack[len(stack)-1] != popped[0]) {
-			stack = append(stack, pushed[0])
-			pushed = pushed[1:]
 		} else if len(stack) > 0 && len(popped) > 0 && stack[len(stack)-1] == popped[0] {
 			stack = stack[:len(stack)-1]
 			popped = popped[1:]
+		} else if len(pushed) > 0 {
+			stack = append(stack, pushed[0])
+			pushed = pushed[1:]
 		} else {
 			return false
 		}
