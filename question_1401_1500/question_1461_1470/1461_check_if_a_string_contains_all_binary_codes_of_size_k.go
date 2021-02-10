7fc04156f02e1a96b2ef75c13bdfcc362ed122b2
lupe <lupe.luo@klook.com>
2021-02-10 21:43:14 +0800

    1451. 重新排列句子中的单词

--- question_1401_1500/question_1461_1470/1461_check_if_a_string_contains_all_binary_codes_of_size_k.go
+++ question_1401_1500/question_1461_1470/1461_check_if_a_string_contains_all_binary_codes_of_size_k.go
@@ -5,14 +5,14 @@ package question_1461_1470
 // Topics: 排序 字符串
 
 func hasAllCodes(s string, k int) bool {
-	var flag, num = make(map[uint32]struct{}), uint32(0)
+	var flag, tmp, num = make(map[int32]struct{}), int32(0), int32(0)
 	for i := 0; i < k && i < len(s); i++ {
-		num = num<<1 + uint32(s[i]-'0')
+		num, tmp = num<<1+int32(s[i]-'0'), tmp<<1+1
 	}
 	flag[num] = struct{}{}
 
 	for i := k; i < len(s); i++ {
-		num = (num<<(33-k))>>(32-k) + uint32(s[i]-'0')
+		num = num<<1&tmp + int32(s[i]-'0')
 		flag[num] = struct{}{}
 	}
 	return len(flag) == 1<<k
