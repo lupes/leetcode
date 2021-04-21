d6b54545cb2e8f5f1d1fb78371c9a56b7817b8f8
lupe <lupe.luo@klook.com>
2021-04-21 19:52:36 +0800

    1629. 按键持续时间最长的键

--- question_1601_1700/question_1621_1630/1629_slowest_key.go
+++ question_1601_1700/question_1621_1630/1629_slowest_key.go
@@ -5,19 +5,11 @@ package question_1621_1630
 // Topics: 数组
 
 func slowestKey(releaseTimes []int, keysPressed string) byte {
-	var flag [26]int
-	var d = 0
-	flag[keysPressed[0]-'a'] = releaseTimes[0]
+	var max, res, d = releaseTimes[0], keysPressed[0], 0
 	for i := 1; i < len(releaseTimes); i++ {
 		d = releaseTimes[i] - releaseTimes[i-1]
-		if d > flag[keysPressed[i]-'a'] {
-			flag[keysPressed[i]-'a'] = d
-		}
-	}
-	var max, res = 0, byte(0)
-	for i, num := range flag {
-		if num >= max {
-			res, max = byte(i)+'a', num
+		if d > max || (d == max && res < keysPressed[i]) {
+			max, res = d, keysPressed[i]
 		}
 	}
 	return res
