bd0f1a616f6c6a0bdcd880277f9ae1c2bf7e339d
lupe <lupe.luo@klook.com>
2021-06-07 19:41:14 +0800

    423. 从英文中重建数字

--- question_401_500/question_421_430/423_reconstruct_original_digits_from_english.go
+++ question_401_500/question_421_430/423_reconstruct_original_digits_from_english.go
@@ -10,7 +10,7 @@ import (
 
 func originalDigits(s string) string {
 	// zero one     two three four five six seven eight nine
-	// z    o-0-2-4 w   h-8   u    v-7  x   s-6   g     i-5-6-8
+	// z    o-0-2-4 w   h-8   u    f-4  x   s-6   g     i-5-6-8
 	var flag, res, nums = make([]int, 'z'+1), bytes.Buffer{}, make([]int, 10)
 	for _, c := range s {
 		flag[c]++
@@ -19,9 +19,9 @@ func originalDigits(s string) string {
 	nums[2] = flag['w']
 	nums[4] = flag['u']
 	nums[1] = flag['o'] - nums[0] - nums[2] - nums[4]
+	nums[5] = flag['f'] - nums[4]
 	nums[6] = flag['x']
 	nums[7] = flag['s'] - nums[6]
-	nums[5] = flag['v'] - nums[7]
 	nums[8] = flag['g']
 	nums[3] = flag['h'] - nums[8]
 	nums[9] = flag['i'] - nums[5] - nums[6] - nums[8]
