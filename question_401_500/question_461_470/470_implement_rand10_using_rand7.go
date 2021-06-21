0c66bd7b0f60e18fb8fd93db475b78835fcdfa39
lupe <lupe.luo@klook.com>
2021-06-21 20:56:56 +0800

    388. 文件的最长绝对路径

--- question_401_500/question_461_470/470_implement_rand10_using_rand7.go
+++ question_401_500/question_461_470/470_implement_rand10_using_rand7.go
@@ -1,9 +1,23 @@
 package question_461_470
 
+import (
+	"math/rand"
+	"time"
+)
+
 // 470. 用 Rand7() 实现 Rand10()
 // https://leetcode-cn.com/problems/implement-rand10-using-rand7
-// Topics: None None
+// Topics: 随机 拒绝采样
+
+func rand7() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(7)
+}
 
 func rand10() int {
-	return 0
+	a, b := rand7(), rand7()
+	if a > 4 && b < 4 {
+		return rand10()
+	}
+	return (a+b)%10 + 1
 }
---
+++ question_401_500/question_461_470/470_implement_rand10_using_rand7_test.go
@@ -0,0 +1,12 @@
+package question_461_470
+
+import (
+	"testing"
+)
+
+func Test_rand10(t *testing.T) {
+	t.Logf("rand10() = %v\n", rand10())
+	t.Logf("rand10() = %v\n", rand10())
+	t.Logf("rand10() = %v\n", rand10())
+	t.Logf("rand10() = %v\n", rand10())
+}
