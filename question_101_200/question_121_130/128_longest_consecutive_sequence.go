d4e69eb1b348d69a1904a4ef97742797fb101d64
lupe <lupe.luo@klook.com>
2021-06-27 21:27:51 +0800

    128. 最长连续序列

--- question_101_200/question_121_130/128_longest_consecutive_sequence.go
+++ question_101_200/question_121_130/128_longest_consecutive_sequence.go
@@ -5,25 +5,15 @@ package question_121_130
 // Topics: 并查集 数组
 
 func longestConsecutive(nums []int) int {
-	var res, min, max, flag = 0, make(map[int]int), make(map[int]int), make(map[int]struct{})
+	var res, flag = 0, make(map[int]int)
 	for _, n := range nums {
 		if _, ok := flag[n]; !ok {
-			start, end := n, n+1
-			if t, ok := min[n+1]; ok {
-				delete(min, n+1)
-				delete(max, t)
-				end = t
-			}
-			if t, ok := max[n]; ok {
-				delete(max, n)
-				delete(min, t)
-				start = t
-			}
-			min[start] = end
-			max[end] = start
-			flag[n] = struct{}{}
-			if end-start > res {
-				res = end - start
+			left, right := flag[n-1], flag[n+1]
+			flag[n] = left + right + 1
+			flag[n-left] = flag[n]
+			flag[n+right] = flag[n]
+			if flag[n] > res {
+				res = flag[n]
 			}
 		}
 	}
