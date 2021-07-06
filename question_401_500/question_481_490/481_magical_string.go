a85eedd86e978549a5329e715fb3a032442e2c87
lupe <lupe.luo@klook.com>
2021-07-06 19:25:21 +0800

    481. 神奇字符串

--- question_401_500/question_481_490/481_magical_string.go
+++ question_401_500/question_481_490/481_magical_string.go
@@ -5,5 +5,19 @@ package question_481_490
 // Topics:
 
 func magicalString(n int) int {
-
+	var res, flag = 1, make([]byte, 0, n+1)
+	flag = append(flag, 1, 2, 2)
+	for low, fast := 2, 3; fast < n; low++ {
+		for i := 0; i < int(flag[low]); i++ {
+			flag = append(flag, flag[fast-1]^3)
+		}
+		if flag[fast-1] == 2 {
+			res += int(flag[low])
+		}
+		fast += int(flag[low])
+	}
+	if len(flag) > n && flag[n] == 1 {
+		res -= 1
+	}
+	return res
 }
