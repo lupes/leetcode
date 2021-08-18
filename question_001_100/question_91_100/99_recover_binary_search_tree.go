0a9b7f294e55bf20c62caa4dccadd4b896a41611
lupe <lupe.luo@klook.com>
2021-08-18 19:36:50 +0800

    99. 恢复二叉搜索树

--- question_001_100/question_91_100/99_recover_binary_search_tree.go
+++ question_001_100/question_91_100/99_recover_binary_search_tree.go
@@ -1,8 +1,6 @@
 package question_91_100
 
 import (
-	"sort"
-
 	. "github.com/lupes/leetcode/common"
 )
 
@@ -12,46 +10,22 @@ import (
 
 func recoverTree(root *TreeNode) {
 	nodes := innerOrder(root)
-	var nums = make([]int, 0, len(nodes))
-	for _, node := range nodes {
-		nums = append(nums, node.Val)
-	}
-	sort.Ints(nums)
 
-	var a, b *TreeNode
-	for i := 0; i < len(nodes); i++ {
-		if nodes[i].Val != nums[i] {
-			if a == nil {
-				a = nodes[i]
-			} else {
-				b = nodes[i]
-				break
-			}
+	var big, small *TreeNode
+	for i := 0; i < len(nodes)-1; i++ {
+		if (i == 0 && nodes[i].Val > nodes[i+1].Val) || (i > 0 && nodes[i].Val > nodes[i-1].Val && nodes[i].Val > nodes[i+1].Val) {
+			big = nodes[i]
+			break
 		}
 	}
-	a.Val, b.Val = b.Val, a.Val
-}
 
-// TODO: 有问题
-func recoverTree2(root *TreeNode) {
-	nodes := innerOrder(root)
-
-	var a, b *TreeNode
-	for i := range nodes {
-		if i == 0 && nodes[i].Val > nodes[i+1].Val {
-			a = nodes[i]
-		} else if i == len(nodes)-1 && nodes[i].Val < nodes[i-1].Val {
-			b = nodes[i]
-		} else if i > 0 && i < len(nodes)-1 {
-			if nodes[i].Val > nodes[i-1].Val && nodes[i].Val > nodes[i+1].Val {
-				a = nodes[i]
-			} else if nodes[i].Val < nodes[i-1].Val && nodes[i].Val < nodes[i+1].Val {
-				b = nodes[i]
-				break
-			}
+	for i := len(nodes) - 1; i > 0; i-- {
+		if (i == len(nodes)-1 && nodes[i].Val < nodes[i-1].Val) || (i < len(nodes)-1 && nodes[i].Val < nodes[i-1].Val && nodes[i].Val < nodes[i+1].Val) {
+			small = nodes[i]
+			break
 		}
 	}
-	a.Val, b.Val = b.Val, a.Val
+	big.Val, small.Val = small.Val, big.Val
 }
 
 func innerOrder(root *TreeNode) []*TreeNode {
