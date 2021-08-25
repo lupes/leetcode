78def51aac2a579e4a3013e89d015ba7f99ba636
lupe <lupe.luo@klook.com>
2021-08-25 18:58:41 +0800

    797. 所有可能的路径

--- question_701_800/question_791_800/797_all_paths_from_source_to_target.go
+++ question_701_800/question_791_800/797_all_paths_from_source_to_target.go
@@ -2,7 +2,7 @@ package question_791_800
 
 // 797. 所有可能的路径
 // https://leetcode-cn.com/problems/all-paths-from-source-to-target
-// Topics:
+// Topics: 深度优先搜索 广度优先搜索 图 回溯
 
 func allPathsSourceTarget(graph [][]int) [][]int {
 	return allPathsSourceTargetHelper(graph, 0, len(graph)-1)
