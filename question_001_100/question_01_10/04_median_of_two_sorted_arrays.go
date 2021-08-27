23a9c267e7cf1fbde120ce3174d7343b8ac609c3
lupe <lupe.luo@klook.com>
2021-08-27 21:18:45 +0800

    4. 寻找两个有序数组的中位数

--- question_001_100/question_01_10/04_median_of_two_sorted_arrays.go
+++ question_001_100/question_01_10/04_median_of_two_sorted_arrays.go
@@ -1,23 +1,31 @@
 package question_01_10
 
-import (
-	"sort"
-)
-
 // 4. 寻找两个有序数组的中位数
 // https://leetcode-cn.com/problems/median-of-two-sorted-arrays
 // Topics: 数组 二分查找 分治算法
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	nums3 := append(nums1, nums2...)
-	sort.Ints(nums3)
-	size := len(nums3)
-	rem, quo := size%2, size/2
-	median := 0.0
-	if rem == 0 {
-		median = float64(nums3[quo-1]+nums3[quo]) / 2
-	} else {
-		median = float64(nums3[quo])
+	l1, l2, l := len(nums1), len(nums2), len(nums1)+len(nums2)
+
+	a, b, t := 0, 0, 0
+	for i1, i2, i3 := 0, 0, 0; i3 < l; i3++ {
+		if l1 == i1 || (l1 > i1 && l2 > i2 && nums1[i1] >= nums2[i2]) {
+			t = nums2[i2]
+			i2++
+		} else if l2 == i2 || (l1 > i1 && l2 > i2 && nums1[i1] < nums2[i2]) {
+			t = nums1[i1]
+			i1++
+		}
+		if i3 == l/2-1 {
+			a = t
+		} else if i3 == l/2 {
+			b = t
+			break
+		}
 	}
-	return median
+	if l&1 == 0 {
+		return float64(a+b) / 2
+	} else {
+		return float64(b)
+	}
 }
--- question_001_100/question_01_10/04_median_of_two_sorted_arrays_test.go
+++ question_001_100/question_01_10/04_median_of_two_sorted_arrays_test.go
@@ -3,22 +3,24 @@ package question_01_10
 import "testing"
 
 func Test_findMedianSortedArrays(t *testing.T) {
-	type args struct {
+	tests := []struct {
 		nums1 []int
 		nums2 []int
-	}
-	tests := []struct {
-		name string
-		args args
-		want float64
+		want  float64
 	}{
-		{"test#1", args{[]int{1, 3}, []int{2}}, 2},
-		{"test#2", args{[]int{1, 2}, []int{3, 4}}, 2.5},
-		{"test#3", args{[]int{1, 3}, []int{}}, 2},
+		{[]int{1, 3}, []int{2}, 2},
+		{[]int{1, 2}, []int{3, 4}, 2.5},
+		{[]int{1, 3}, []int{}, 2},
+		{[]int{1}, []int{}, 1},
+		{[]int{}, []int{1}, 1},
+		{[]int{}, []int{1}, 1},
+		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}, 3},
+		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5, 6}, 3},
+		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5, 6, 7}, 3},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := findMedianSortedArrays(tt.args.nums1, tt.args.nums2); got != tt.want {
+		t.Run("test", func(t *testing.T) {
+			if got := findMedianSortedArrays(tt.nums1, tt.nums2); got != tt.want {
 				t.Errorf("findMedianSortedArrays() = %v, want %v", got, tt.want)
 			}
 		})
