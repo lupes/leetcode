915197d33b097fc81994cd148a8f30ed1c61f115
lupe <lupe.luo@klook.com>
2021-09-09 19:31:31 +0800

    68. 文本左右对齐

--- question_001_100/question_61_70/68_text_justification.go
+++ question_001_100/question_61_70/68_text_justification.go
@@ -6,55 +6,37 @@ import (
 
 // 68. 文本左右对齐
 // https://leetcode-cn.com/problems/text-justification
-// Topics: 字符串
+// Topics: 字符串 模拟
 
 func fullJustify(words []string, maxWidth int) []string {
-	var line []string
-	size := len(words)
-	for start := 0; start <= size; {
-		last := start
-		for ; last < size; last++ {
-			if len(strings.Join(words[start:last+1], " ")) > maxWidth {
-				break
-			}
-		}
-		if last < size {
-			s := align(words[start:last], maxWidth)
-			line = append(line, s)
-		} else {
-			s := align(words[start:last], maxWidth, true)
-			line = append(line, s)
+	var lines []string
+	var l = 0
+	for left, right := 0, 0; right < len(words); {
+		if right == len(words)-1 && l+len(words[right]) < maxWidth {
+			s := strings.Join(words[left:right+1], " ")
+			s += strings.Repeat(" ", maxWidth-len(s))
+			lines = append(lines, s)
 			break
+		} else if l+len(words[right]) < maxWidth {
+			l += len(words[right]) + 1
+			right++
+		} else if l+len(words[right]) == maxWidth {
+			lines = append(lines, strings.Join(words[left:right+1], " "))
+			l = 0
+			right++
+			left = right
+		} else {
+			s := align(words[left:right], maxWidth)
+			lines = append(lines, s)
+			l = 0
+			left = right
 		}
-		start = last
 	}
-	//for i := 0; i < size; i++ {
-	//	tmp = append(tmp, words[i])
-	//	s := strings.Join(tmp, " ")
-	//	if len(s) > maxWidth {
-	//		tmp = tmp[:len(tmp)-1]
-	//		if i < size-1 {
-	//			s := align(tmp, maxWidth)
-	//			line = append(line, s)
-	//		} else {
-	//			s := align(tmp, maxWidth)
-	//			line = append(line, s)
-	//			s = align([]string{words[i]}, maxWidth, true)
-	//			line = append(line, s)
-	//		}
-	//		tmp = []string{words[i]}
-	//	} else if i == size-1 {
-	//		s := align(tmp, maxWidth, true)
-	//		line = append(line, s)
-	//	}
-	//}
-	return line
+
+	return lines
 }
 
-func align(words []string, width int, last ...bool) string {
-	if len(last) == 1 && last[0] {
-		words = []string{strings.Join(words, " ")}
-	}
+func align(words []string, width int) string {
 	l := len(words)
 	if l == 1 {
 		return words[0] + strings.Repeat(" ", width-len(words[0]))
@@ -66,9 +48,7 @@ func align(words []string, width int, last ...bool) string {
 	for i, word := range words {
 		if i == len(words)-1 {
 			s += word
-			break
-		}
-		if i < re {
+		} else if i < re {
 			s += word + strings.Repeat(" ", qu+1)
 		} else {
 			s += word + strings.Repeat(" ", qu)
--- question_001_100/question_61_70/68_text_justification_test.go
+++ question_001_100/question_61_70/68_text_justification_test.go
@@ -7,34 +7,27 @@ import (
 )
 
 func Test_fullJustify(t *testing.T) {
-	type args struct {
+	tests := []struct {
 		words    []string
 		maxWidth int
-	}
-	tests := []struct {
-		name string
-		args args
-		want []string
+		want     []string
 	}{
 		{
-			"test#1",
-			args{[]string{"This", "is", "an", "example", "of", "text", "justification."}, 16},
+			[]string{"This", "is", "an", "example", "of", "text", "justification."}, 16,
 			[]string{"This    is    an", "example  of text", "justification.  "},
 		},
 		{
-			"test#2",
-			args{[]string{"What", "must", "be", "acknowledgment", "shall", "be"}, 16},
+			[]string{"What", "must", "be", "acknowledgment", "shall", "be"}, 16,
 			[]string{"What   must   be", "acknowledgment  ", "shall be        "},
 		},
 		{
-			"test#3",
-			args{[]string{"Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"}, 20},
+			[]string{"Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"}, 20,
 			[]string{"Science  is  what we", "understand      well", "enough to explain to", "a  computer.  Art is", "everything  else  we", "do                  "},
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := fullJustify(tt.args.words, tt.args.maxWidth); !reflect.DeepEqual(got, tt.want) {
+		t.Run("test", func(t *testing.T) {
+			if got := fullJustify(tt.words, tt.maxWidth); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("fullJustify() \n[%v], \n\n[%v]", strings.Join(got, "]\n["), strings.Join(tt.want, "]\n["))
 			}
 		})
