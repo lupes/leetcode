1ad48377190630ebc20759452546fa78fc8ecf67
lupe <lupe.luo@klook.com>
2021-09-21 19:18:34 +0800

    58. 最后一个单词的长度

--- question_001_100/question_51_60/58_length_of_last_word.go
+++ question_001_100/question_51_60/58_length_of_last_word.go
@@ -1,21 +1,18 @@
 package question_51_60
 
-import "strings"
-
 // 58. 最后一个单词的长度
 // https://leetcode-cn.com/problems/length-of-last-word
 // Topics: 字符串
 
 func lengthOfLastWord(s string) int {
-	if s == "" {
-		return 0
-	}
-	s = strings.TrimRight(s, " ")
-	size := len(s)
-	for i := size - 1; i >= 0; i-- {
-		if s[i] == ' ' {
-			return len(s[i+1:])
+	var last, tmp = 0, 0
+	for _, c := range s {
+		if c == ' ' {
+			tmp = 0
+		} else {
+			tmp += 1
+			last = tmp
 		}
 	}
-	return size
+	return last
 }
--- question_001_100/question_51_60/58_length_of_last_word_test.go
+++ question_001_100/question_51_60/58_length_of_last_word_test.go
@@ -4,18 +4,18 @@ import "testing"
 
 func Test_lengthOfLastWord(t *testing.T) {
 	tests := []struct {
-		name string
 		s    string
 		want int
 	}{
-		{"test#0", "", 0},
-		{"test#1", " ", 0},
-		{"test#2", "hello", 5},
-		{"test#3", "hello world", 5},
-		{"test#4", "hello world ", 5},
+		{"", 0},
+		{" ", 0},
+		{"hello", 5},
+		{"hello world", 5},
+		{"   fly me   to   the moon  ", 4},
+		{"luffy is still joyboy", 6},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run("test", func(t *testing.T) {
 			if got := lengthOfLastWord(tt.s); got != tt.want {
 				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
 			}
