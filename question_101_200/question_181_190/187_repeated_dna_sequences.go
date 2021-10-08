8259263b902c421b8e5785aeedf06b84ede4ca0b
lupe <lupe.luo@klook.com>
2021-10-08 19:01:07 +0800

    187. 重复的DNA序列

--- question_101_200/question_181_190/187_repeated_dna_sequences.go
+++ question_101_200/question_181_190/187_repeated_dna_sequences.go
@@ -6,17 +6,33 @@ package question_181_190
 
 func findRepeatedDnaSequences(s string) []string {
 	var res []string
-	var tmp = make(map[string]bool)
-	var key string
+	var tmp = make(map[int]bool)
 	for i := 0; i < len(s)-9; i++ {
-		key = s[i : i+10]
+		key := hash(s[i : i+10])
 		if b, ok := tmp[key]; ok {
 			if b {
-				res = append(res, key)
+				res = append(res, s[i:i+10])
 				tmp[key] = false
 			}
 		} else {
 			tmp[key] = true
+		}
+	}
+	return res
+}
+
+func hash(s string) int {
+	var res = 0
+	for _, c := range s {
+		switch c {
+		case 'A':
+			res = res << 2
+		case 'C':
+			res = res<<2 + 1
+		case 'G':
+			res = res<<2 + 2
+		case 'T':
+			res = res<<2 + 3
 		}
 	}
 	return res
