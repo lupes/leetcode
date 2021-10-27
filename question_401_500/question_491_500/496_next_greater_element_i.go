31972f9a73b607c5cb7cc58ed4cd6e5d55e0be06
lupe <lupe.luo@klook.com>
2021-10-27 10:05:52 +0800

    496. 下一个更大元素 I

--- question_401_500/question_491_500/496_next_greater_element_i.go
+++ question_401_500/question_491_500/496_next_greater_element_i.go
@@ -2,18 +2,26 @@ package question_491_500
 
 // 496. 下一个更大元素 I
 // https://leetcode-cn.com/problems/next-greater-element-i/
-// Topics: 栈
+// Topics: 栈 数组 哈希表 单调栈
 
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
-	var res = make([]int, len(nums1))
-	var l = len(nums2)
-	for i, n1 := range nums1 {
-		res[i] = -1
-		for j := l - 1; j > 0 && nums2[j] != n1; j-- {
-			if nums2[j] > n1 {
-				res[i] = nums2[j]
-			}
+	var stack []int
+	var flag = make(map[int]int, len(nums2))
+	for i := len(nums2) - 1; i >= 0; i-- {
+		for len(stack) > 0 && stack[len(stack)-1] < nums2[i] {
+			stack = stack[:len(stack)-1]
 		}
+		if len(stack) == 0 {
+			flag[nums2[i]] = -1
+		} else {
+			flag[nums2[i]] = stack[len(stack)-1]
+		}
+		stack = append(stack, nums2[i])
 	}
-	return res
+
+	for i, n := range nums1 {
+		nums1[i] = flag[n]
+	}
+
+	return nums1
 }
