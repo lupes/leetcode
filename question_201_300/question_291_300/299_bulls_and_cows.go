b13709b8b5098570f5137614f9dc7f0cce127bf2
lupe <lupe.luo@klook.com>
2021-11-08 18:36:45 +0800

    299. 猜数字游戏

--- question_201_300/question_291_300/299_bulls_and_cows.go
+++ question_201_300/question_291_300/299_bulls_and_cows.go
@@ -4,24 +4,24 @@ import "fmt"
 
 // 299. 猜数字游戏
 // https://leetcode-cn.com/problems/bulls-and-cows/
-// Topics: 哈希表
+// Topics: 哈希表 字符串 计数
 
 func getHint(secret string, guess string) string {
 	a, b := 0, 0
-	sm, gm := make(map[uint8]int), make(map[uint8]int)
+	var flag [10][2]int
 	for i := 0; i < len(secret); i++ {
 		if secret[i] == guess[i] {
 			a++
 		} else {
-			sm[secret[i]]++
-			gm[guess[i]]++
+			flag[secret[i]-'0'][0]++
+			flag[guess[i]-'0'][1]++
 		}
 	}
-	for i := uint8('0'); i <= '9'; i++ {
-		if sm[i] > gm[i] {
-			b += gm[i]
+	for i := 0; i <= 9; i++ {
+		if flag[i][0] > flag[i][1] {
+			b += flag[i][1]
 		} else {
-			b += sm[i]
+			b += flag[i][0]
 		}
 	}
 	return fmt.Sprintf("%dA%dB", a, b)
--- question_201_300/question_291_300/299_bulls_and_cows_test.go
+++ question_201_300/question_291_300/299_bulls_and_cows_test.go
@@ -4,16 +4,15 @@ import "testing"
 
 func Test_getHint(t *testing.T) {
 	tests := []struct {
-		name   string
 		secret string
 		guess  string
 		want   string
 	}{
-		{"test", "1807", "7810", "1A3B"},
-		{"test", "1123", "0111", "1A1B"},
+		{"1807", "7810", "1A3B"},
+		{"1123", "0111", "1A1B"},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run("test", func(t *testing.T) {
 			if got := getHint(tt.secret, tt.guess); got != tt.want {
 				t.Errorf("getHint() = %v, want %v", got, tt.want)
 			}
