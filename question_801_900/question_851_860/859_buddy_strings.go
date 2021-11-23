73af3d86ff371f4d347e8523becac200be35d154
lupe <lupe.luo@klook.com>
2021-11-23 18:47:20 +0800

    859. 亲密字符串

--- question_801_900/question_851_860/859_buddy_strings.go
+++ question_801_900/question_851_860/859_buddy_strings.go
@@ -2,35 +2,34 @@ package question_851_860
 
 // 859. 亲密字符串
 // https://leetcode-cn.com/problems/buddy-strings
-// Topics: 字符串
+// Topics: 字符串 哈希表
 
 func buddyStrings(A string, B string) bool {
 	if len(A) != len(B) {
 		return false
 	}
-	var num = 0
-	var a, b byte
+	var a, b, num int
 	var flag = make([]int, 26)
-	var t = false
 	for i := range A {
 		if A[i] != B[i] {
 			num++
 			if num == 1 {
-				a, b = A[i], B[i]
-			} else if num == 2 && (a != B[i] || b != A[i]) {
-				return false
+				a = i
+			} else if num == 2 {
+				b = i
 			} else if num == 3 {
 				return false
 			}
 		} else {
 			flag[A[i]-'a']++
-			if flag[A[i]-'a'] >= 2 {
-				t = true
+		}
+	}
+	if num == 0 {
+		for _, t := range flag {
+			if t > 1 {
+				return true
 			}
 		}
 	}
-	if num == 0 && t {
-		return true
-	}
-	return num == 2
+	return num == 2 && A[a] == B[b] && A[b] == B[a]
 }
