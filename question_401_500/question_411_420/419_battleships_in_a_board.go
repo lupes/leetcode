f5a84a2fdb5031d16f8e0ceaa73901c5f69be56d
lupe <lupe.luo@klook.com>
2021-12-18 18:33:29 +0800

    419. 甲板上的战舰

--- question_401_500/question_411_420/419_battleships_in_a_board.go
+++ question_401_500/question_411_420/419_battleships_in_a_board.go
@@ -8,11 +8,8 @@ func countBattleships(board [][]byte) int {
 	var res = 0
 	for i, row := range board {
 		for j, c := range row {
-			if c == 'X' {
-				if (i == 0 || (i > 0 && board[i-1][j] == '.')) && (j == 0 || (j > 0 && board[i][j-1] == '.')) {
-					res++
-				}
-				board[i][j] = '+'
+			if c == 'X' && (i == 0 || (i > 0 && board[i-1][j] == '.')) && (j == 0 || (j > 0 && board[i][j-1] == '.')) {
+				res++
 			}
 		}
 	}
